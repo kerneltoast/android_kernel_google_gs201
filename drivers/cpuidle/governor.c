1b9195e4a04630b3876244ed8d8ab2f7335b5c9a
Praveen Chidambaram <pchidamb@codeaurora.org>
2014-05-21 16:21:31 -0600

    QoS: Enhance framework to support cpu/irq specific QoS requests
    QoS request for CPU_DMA_LATENCY can be better optimized if the request
    can be set only for the required cpus and not all cpus. This helps save
    power on other cores, while still gauranteeing the quality of service.
    
    Enhance the QoS constraints data structures to support target value for
    each core. Requests specify if the QoS is applicable to all cores
    (default) or to a selective subset of the cores or to a core(s), that the
    IRQ is affine to.
    
    QoS requests that need to track an IRQ can be set to apply only on the
    cpus to which the IRQ's smp_affinity attribute is set to. The QoS
    framework will automatically track IRQ migration between the cores. The
    QoS is updated to be applied only to the core(s) that the IRQ has been
    migrated to.
    
    Idle and interested drivers can request a PM QoS value for a constraint
    across all cpus, or a specific cpu or a set of cpus. Separate APIs have
    been added to request for individual cpu or a cpumask.  The default
    behaviour of PM QoS is maintained i.e, requests that do not specify a
    type of the request will continue to be effected on all cores.  Requests
    that want to specify an affinity of cpu(s) or an irq, can modify the PM
    QoS request data structures by specifying the type of the request and
    either the mask of the cpus or the IRQ number depending on the type.
    Updating the request does not reset the type of the request.
    
    The userspace sysfs interface does not support CPU/IRQ affinity.
    
    Change-Id: I09ae85a1e8585d44440e86d63504ad734e8e3e36
    Signed-off-by: Praveen Chidambaram <pchidamb@codeaurora.org>
    [Sultan Alsawaf: selectively forward port to 5.10]
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/cpuidle/governor.c
+++ drivers/cpuidle/governor.c
@@ -112,10 +112,10 @@ s64 cpuidle_governor_latency_req(unsigned int cpu)
 {
 	struct device *device = get_cpu_device(cpu);
 	int device_req = dev_pm_qos_raw_resume_latency(device);
-	int global_req = cpu_latency_qos_limit();
+	int cpu_req = cpu_latency_qos_limit(cpu);
 
-	if (device_req > global_req)
-		device_req = global_req;
+	if (device_req > cpu_req)
+		device_req = cpu_req;
 
 	return (s64)device_req * NSEC_PER_USEC;
 }
