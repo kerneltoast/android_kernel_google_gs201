892cfb31261f8dbdd630b5d15159f29a1fad3c3a
Chenglu Lin <chenglulin@google.com>
2018-09-28 16:03:27 +0800

    driver: keydebug: Enhance long press key debugging capability
    Add debug features
      - Dump top CPU usage tasks.
      - Dump uninterruptible tasks
      - Dump call stack of each online core
    
    Bug: 155354637
    Bug: 168751003
    Change-Id: I3ddd2e0addf3982b37decefbd0c5eaeebd6ada0f
    Signed-off-by: Chenglu Lin <chenglulin@google.com>
    Signed-off-by: Jonglin Lee <jonglin@google.com>
    Signed-off-by: Woody Lin <woodylin@google.com>
    (cherry picked from commit e4f758f9cd0acfd1af188b6718d2b6c07caa18a3)

---
+++ include/linux/keydebug-func.h
@@ -0,0 +1,25 @@
+/*
+ * include/linux/keydebug-func.h - function and structure for debug
+ * functions used by keydebug driver
+ *
+ * Copyright (C) 2018 Google, Inc.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef _LINUX_KEYDEBUG_FUNC_H
+#define _LINUX_KEYDEBUG_FUNC_H
+
+void kernel_top_monitor(void);
+void kernel_top_init(void);
+void kernel_top_exit(void);
+
+#endif /* _LINUX_KEYDEBUG_FUNC_H */
