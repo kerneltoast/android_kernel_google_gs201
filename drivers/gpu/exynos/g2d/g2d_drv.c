2587ff411499c6f8c50489c490e5263207e8f083
hyesoo.yu <hyesoo.yu@samsung.com>
2019-02-14 17:05:45 +0900

    g2d: add performance debugging information on stamp
    If the frame drop occurs, there are two causes.
    One is time to wait fence, and the other is actually
    hardware operation time. To figure out where the
    problem is, the information such as fence's name,
    H/W clock before and after H/W runs is added on stamp.
    
    Change-Id: I438b808f1f044875d39df244432a043b54f9bdd7
    Signed-off-by: hyesoo.yu <hyesoo.yu@samsung.com>
    (cherry picked from commit 8f817a8adf26652641f18f1f17facaaf52cde0b9)
    Signed-off-by: Mark Salyzyn <salyzyn@google.com>
    Bug: 165562344

--- drivers/gpu/exynos/g2d/g2d_drv.c
+++ drivers/gpu/exynos/g2d/g2d_drv.c
@@ -437,7 +437,8 @@ static long g2d_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 			break;
 		}
 
-		g2d_stamp_task(task, G2D_STAMP_STATE_BEGIN, task->sec.priority);
+		g2d_stamp_task(task, G2D_STAMP_STATE_BEGIN,
+			       atomic_read(&task->starter.refcount.refs));
 
 		g2d_start_task(task);
 
