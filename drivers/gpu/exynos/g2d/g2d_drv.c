211d420a28af5bab5b3cbfaa46065838619df419
Hyesoo Yu <hyesoo.yu@samsung.com>
2020-01-23 18:04:23 +0900

    g2d: move the g2d_finish_task after dump SFR
    The device power is turned off on g2d_finish_task,
    but there is the access of SFR after g2d_finish_task.
    We move the function, and unify two function to finish
    the task into one function.
    
    Change-Id: I732b6a476c532d6be1e40dca953f12cb5857069c
    Signed-off-by: Hyesoo Yu <hyesoo.yu@samsung.com>
    Signed-off-by: Cho KyongHo <pullip.cho@samsung.com>
    (cherry picked from commit 102f9ddcf835a3d4bf1fa9c4d4db90983b2eca1f)
    Signed-off-by: Mark Salyzyn <salyzyn@google.com>
    Bug: 165562344

--- drivers/gpu/exynos/g2d/g2d_drv.c
+++ drivers/gpu/exynos/g2d/g2d_drv.c
@@ -101,12 +101,12 @@ void g2d_hw_timeout_handler(struct timer_list *arg)
 
 	g2d_dump_info(g2d_dev, task);
 
-	g2d_flush_all_tasks(g2d_dev);
-
 	ret = g2d_hw_global_reset(g2d_dev);
 
 	perrdev(g2d_dev, "GLOBAL RESET %s : H/W timeout",
 		ret ? "SUCCESS" : "FAIL");
+
+	g2d_finish_tasks(g2d_dev, 0, false);
 
 	spin_unlock_irqrestore(&g2d_dev->lock_task, flags);
 
@@ -136,25 +136,14 @@ int g2d_device_run(struct g2d_device *g2d_dev, struct g2d_task *task)
 static irqreturn_t g2d_irq_handler(int irq, void *priv)
 {
 	struct g2d_device *g2d_dev = priv;
-	unsigned int id;
 	u32 intflags, errstatus;
 
 	spin_lock(&g2d_dev->lock_task);
 
 	intflags = g2d_hw_finished_job_ids(g2d_dev);
+	g2d_hw_clear_job_ids(g2d_dev, intflags);
 
 	g2d_stamp_task(NULL, G2D_STAMP_STATE_INT, intflags);
-
-	if (intflags != 0) {
-		for (id = 0; id < G2D_MAX_JOBS; id++) {
-			if ((intflags & (1 << id)) == 0)
-				continue;
-
-			g2d_finish_task_with_id(g2d_dev, id, true);
-		}
-
-		g2d_hw_clear_job_ids(g2d_dev, intflags);
-	}
 
 	errstatus = g2d_hw_errint_status(g2d_dev);
 	if (errstatus != 0) {
@@ -177,13 +166,13 @@ static irqreturn_t g2d_irq_handler(int irq, void *priv)
 
 		g2d_dump_info(g2d_dev, task);
 
-		g2d_flush_all_tasks(g2d_dev);
-
 		ret = g2d_hw_global_reset(g2d_dev);
 
 		perrdev(g2d_dev, "GLOBAL RESET %s : error interrupt",
 			ret ? "SUCCESS" : "FAIL");
 	}
+
+	g2d_finish_tasks(g2d_dev, intflags, !errstatus);
 
 	spin_unlock(&g2d_dev->lock_task);
 
