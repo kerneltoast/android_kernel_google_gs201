a2db64551688abf2588d3460f95d1f23103651bc
Ayoung Sim <a.sim@samsung.com>
2020-02-03 16:05:42 +0900

    include: linux: add SBWC lossy macros
    Add SBWC lossy macros and apply height 16 align for SBWC.
    In case of MPEG4 codec, the height size should be align 16.
    So this applies 16 align for height.
    
    Signed-off-by: Sunyoung Kang <sy0816.kang@samsung.com>
    Signed-off-by: Ayoung Sim <a.sim@samsung.com>
    Change-Id: I8cf4f164eb5a77bdab852474bf3074f589677c8a
    Signed-off-by: Will McVicker <willmcvicker@google.com>

--- include/uapi/linux/videodev2_exynos_media.h
+++ include/uapi/linux/videodev2_exynos_media.h
@@ -137,26 +137,26 @@
 #define SBWC_HEADER_STRIDE(w)		((((((w) + 63) / 64) + 15) / 16) * 16)
 
 #define SBWC_8B_Y_SIZE(w, h)						\
-	((SBWC_8B_STRIDE(w) * ((__ALIGN_UP((h), 8) + 3) / 4)) + 64)
+	((SBWC_8B_STRIDE(w) * ((__ALIGN_UP((h), 16) + 3) / 4)) + 64)
 #define SBWC_8B_Y_HEADER_SIZE(w, h)					\
 	__ALIGN_UP(((SBWC_HEADER_STRIDE(w) *				\
-	((__ALIGN_UP((h), 8) + 3) / 4)) + 256), 32)
+	((__ALIGN_UP((h), 16) + 3) / 4)) + 256), 32)
 #define SBWC_8B_CBCR_SIZE(w, h)						\
-	((SBWC_8B_STRIDE(w) * (((__ALIGN_UP((h), 8) / 2) + 3) / 4)) + 64)
+	((SBWC_8B_STRIDE(w) * (((__ALIGN_UP((h), 16) / 2) + 3) / 4)) + 64)
 #define SBWC_8B_CBCR_HEADER_SIZE(w, h)					\
 	((SBWC_HEADER_STRIDE(w) *					\
-	(((__ALIGN_UP((h), 8) / 2) + 3) / 4)) + 128)
+	(((__ALIGN_UP((h), 16) / 2) + 3) / 4)) + 128)
 
 #define SBWC_10B_Y_SIZE(w, h)						\
-	((SBWC_10B_STRIDE(w) * ((__ALIGN_UP((h), 8) + 3) / 4)) + 64)
+	((SBWC_10B_STRIDE(w) * ((__ALIGN_UP((h), 16) + 3) / 4)) + 64)
 #define SBWC_10B_Y_HEADER_SIZE(w, h)					\
-	__ALIGN_UP(((SBWC_HEADER_STRIDE(w) *				\
-	((__ALIGN_UP((h), 8) + 3) / 4)) + 256), 32)
+	__ALIGN_UP((((__ALIGN_UP((w), 16) *				\
+	__ALIGN_UP((h), 16) * 2) + 256) - SBWC_10B_Y_SIZE(w, h)), 32)
 #define SBWC_10B_CBCR_SIZE(w, h)					\
-	((SBWC_10B_STRIDE(w) * (((__ALIGN_UP((h), 8) / 2) + 3) / 4)) + 64)
+	((SBWC_10B_STRIDE(w) * (((__ALIGN_UP((h), 16) / 2) + 3) / 4)) + 64)
 #define SBWC_10B_CBCR_HEADER_SIZE(w, h)					\
-	((SBWC_HEADER_STRIDE(w) *					\
-	(((__ALIGN_UP((h), 8) / 2) + 3) / 4)) + 128)
+	(((__ALIGN_UP((w), 16) *					\
+	__ALIGN_UP((h), 16)) + 256) - SBWC_10B_CBCR_SIZE(w, h))
 
 /* SBWC - single fd */
 #define SBWC_8B_CBCR_BASE(base, w, h)					\
@@ -164,4 +164,22 @@
 #define SBWC_10B_CBCR_BASE(base, w, h)					\
 	((base) + SBWC_10B_Y_SIZE(w, h) + SBWC_10B_Y_HEADER_SIZE(w, h))
 
+/* SBWC Lossy */
+#define SBWCL_8B_STRIDE(w, r)	(((128 * (r)) / 100) * (((w) + 31) / 32))
+#define SBWCL_10B_STRIDE(w, r)	(((160 * (r)) / 100) * (((w) + 31) / 32))
+
+#define SBWCL_8B_Y_SIZE(w, h, r)					\
+	((SBWCL_8B_STRIDE(w, r) * ((__ALIGN_UP((h), 16) + 3) / 4)) + 64)
+#define SBWCL_8B_CBCR_SIZE(w, h, r)					\
+	((SBWCL_8B_STRIDE(w, r) * (((__ALIGN_UP((h), 16) / 2) + 3) / 4)) + 64)
+
+#define SBWCL_10B_Y_SIZE(w, h, r)					\
+	((SBWCL_10B_STRIDE(w, r) * ((__ALIGN_UP((h), 16) + 3) / 4)) + 64)
+#define SBWCL_10B_CBCR_SIZE(w, h, r)					\
+	((SBWCL_10B_STRIDE(w, r) * (((__ALIGN_UP((h), 16) / 2) + 3) / 4)) + 64)
+
+#define SBWCL_8B_CBCR_BASE(base, w, h, r)				\
+	((base) + SBWCL_8B_Y_SIZE(w, h, r))
+#define SBWCL_10B_CBCR_BASE(base, w, h, r)				\
+	((base) + SBWCL_10B_Y_SIZE(w, h, r))
 #endif /* __LINUX_VIDEODEV2_EXYNOS_MEDIA_H */
