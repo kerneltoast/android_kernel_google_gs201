2e613cb247bd3e2691367f735b2d493e4873372e
Badhri Jagan Sridharan <badhri@google.com>
2020-02-20 17:39:48 -0800

    fusb307b: Make checking data capable path optional
    Bug: 145896043
    Change-Id: I63063dc42c96e240af33e31e405054a8479f2f2c
    Signed-off-by: Badhri Jagan Sridharan <badhri@google.com>
    (cherry picked from commit 45043662bbd89d02c55f71bb3a07bf552df745ac)
    (cherry picked from commit 1124a93c3d6b651cd8d477a0f9d48f7172c46382)
    Signed-off-by: J. Avila <elavila@google.com>

--- drivers/usb/typec/tcpm/tcpci_fusb307.c
+++ drivers/usb/typec/tcpm/tcpci_fusb307.c
@@ -63,6 +63,7 @@ struct fusb307b_plat {
 	unsigned int vbus_mv;
 	/* USB Data notification */
 	struct extcon_dev *extcon;
+	bool no_bc_12;
 
 	struct logbuffer *log;
 };
@@ -373,7 +374,8 @@ static void enable_data_path(struct fusb307b_plat *chip)
 	int ret;
 
 	mutex_lock(&chip->data_path_lock);
-	if (chip->attached && chip->data_capable && !chip->data_active) {
+	if (chip->attached && (chip->no_bc_12 || chip->data_capable) &&
+	    !chip->data_active) {
 		ret = extcon_set_state_sync(chip->extcon,
 			chip->data_role == TYPEC_HOST ?
 			EXTCON_USB_HOST : EXTCON_USB, 1);
@@ -525,6 +527,8 @@ static int fusb307b_probe(struct i2c_client *client,
 		ret = -EINVAL;
 		goto unreg_psy;
 	}
+
+	chip->no_bc_12 = of_property_read_bool(dn, "no-bc-12");
 
 	chip->usb_psy = power_supply_get_by_name(usb_psy_name);
 	if (IS_ERR_OR_NULL(chip->usb_psy)) {
