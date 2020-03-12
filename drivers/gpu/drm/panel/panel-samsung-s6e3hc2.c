d6d029341fb0f3e91b78058554ddde8000295d56
Long Ling <longling@google.com>
2020-03-12 14:43:28 -0700

    drivers/drm/panel: support s6e3hc2 fhd panel
    Bug: 149051125
    Signed-off-by: Long Ling <longling@google.com>
    Change-Id: I7ea40a50949818162b8784d5af7c38ff45a9b8aa
    Signed-off-by: Will McVicker <willmcvicker@google.com>

--- drivers/gpu/drm/panel/panel-samsung-s6e3hc2.c
+++ drivers/gpu/drm/panel/panel-samsung-s6e3hc2.c
@@ -13,12 +13,32 @@
 #include <linux/module.h>
 #include <panel-samsung-drv.h>
 
-static unsigned char SEQ_PPS_SLICE2[] = {
+static unsigned char SEQ_PPS_WQHD[] = {
 	// WQHD+ :1440x3040
 	0x11, 0x00, 0x00, 0x89, 0x30, 0x80, 0x0B, 0xE0,
 	0x05, 0xA0, 0x00, 0x28, 0x02, 0xD0, 0x02, 0xD0,
 	0x02, 0x00, 0x02, 0x68, 0x00, 0x20, 0x04, 0x6C,
 	0x00, 0x0A, 0x00, 0x0C, 0x02, 0x77, 0x01, 0xE9,
+	0x18, 0x00, 0x10, 0xF0, 0x03, 0x0C, 0x20, 0x00,
+	0x06, 0x0B, 0x0B, 0x33, 0x0E, 0x1C, 0x2A, 0x38,
+	0x46, 0x54, 0x62, 0x69, 0x70, 0x77, 0x79, 0x7B,
+	0x7D, 0x7E, 0x01, 0x02, 0x01, 0x00, 0x09, 0x40,
+	0x09, 0xBE, 0x19, 0xFC, 0x19, 0xFA, 0x19, 0xF8,
+	0x1A, 0x38, 0x1A, 0x78, 0x1A, 0xB6, 0x2A, 0xF6,
+	0x2B, 0x34, 0x2B, 0x74, 0x3B, 0x74, 0x6B, 0xF4,
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+};
+
+static unsigned char SEQ_PPS_FHD[] = {
+	//FHD+ :1080x2340
+	0x11, 0x00, 0x00, 0x89, 0x30, 0x80, 0x09, 0x24,
+	0x04, 0x38, 0x00, 0x41, 0x02, 0x1C, 0x02, 0x1C,
+	0x02, 0x00, 0x02, 0x0E, 0x00, 0x20, 0x06, 0x50,
+	0x00, 0x07, 0x00, 0x0C, 0x01, 0x80, 0x01, 0x91,
 	0x18, 0x00, 0x10, 0xF0, 0x03, 0x0C, 0x20, 0x00,
 	0x06, 0x0B, 0x0B, 0x33, 0x0E, 0x1C, 0x2A, 0x38,
 	0x46, 0x54, 0x62, 0x69, 0x70, 0x77, 0x79, 0x7B,
@@ -68,6 +88,9 @@ static int s6e3hc2_prepare(struct drm_panel *panel)
 	return 0;
 }
 
+const struct exynos_panel_desc samsung_s6e3hc2_fhd;
+const struct exynos_panel_desc samsung_s6e3hc2_wqhd;
+
 static int s6e3hc2_enable(struct drm_panel *panel)
 {
 	struct exynos_panel *ctx;
@@ -83,7 +106,13 @@ static int s6e3hc2_enable(struct drm_panel *panel)
 
 	/* DSC related configuration */
 	exynos_dcs_compression_mode(ctx, 0x1);
-	EXYNOS_PPS_LONG_WRITE(ctx, SEQ_PPS_SLICE2);
+
+	if (ctx->desc == &samsung_s6e3hc2_wqhd)
+		EXYNOS_PPS_LONG_WRITE(ctx, SEQ_PPS_WQHD);
+	else if (ctx->desc == &samsung_s6e3hc2_fhd)
+		EXYNOS_PPS_LONG_WRITE(ctx, SEQ_PPS_FHD);
+	else
+		dev_warn(ctx->dev, "%s: invalid panel\n", __func__);
 
 	/* sleep out: 120ms delay */
 	EXYNOS_DCS_WRITE_SEQ_DELAY(ctx, 120, 0x11);
@@ -123,7 +152,7 @@ static int s6e3hc2_set_brightness(struct exynos_panel *exynos_panel, u16 br)
 	return exynos_dcs_set_brightness(exynos_panel, brightness);
 }
 
-static const struct drm_display_mode s6e3hc2_mode = {
+static const struct drm_display_mode s6e3hc2_wqhd_mode = {
 	.clock = 56125,	/* 898Mbps / 2 = 449Mhz / 8 = 56.125Mhz */
 	.hdisplay = 1440,
 	.hsync_start = 1440 + 2,
@@ -139,6 +168,22 @@ static const struct drm_display_mode s6e3hc2_mode = {
 	.height_mm = 142,
 };
 
+static const struct drm_display_mode s6e3hc2_fhd_mode = {
+	.clock = 34375,	/* 550Mbps / 2 = 275Mhz / 8 = 34.375Mhz */
+	.hdisplay = 1080,
+	.hsync_start = 1080 + 32, // add hfp
+	.hsync_end = 1080 + 32 + 12, // add hsa
+	.htotal = 1080 + 32 + 12 + 16, // add hbp
+	.vdisplay = 2340,
+	.vsync_start = 2340 + 12, // add vfp
+	.vsync_end = 2340 + 12 + 4, // add vsa
+	.vtotal = 2340 + 12 + 4 + 16, // add vbp
+	.vrefresh = 60,
+	.flags = 0,
+	.width_mm = 63,
+	.height_mm = 137,
+};
+
 static const struct drm_panel_funcs s6e3hc2_drm_funcs = {
 	.disable = s6e3hc2_disable,
 	.unprepare = s6e3hc2_unprepare,
@@ -151,7 +196,7 @@ static const struct exynos_panel_funcs s6e3hc2_exynos_funcs = {
 	.set_brightness = s6e3hc2_set_brightness,
 };
 
-const struct exynos_panel_desc samsung_s6e3hc2 = {
+const struct exynos_panel_desc samsung_s6e3hc2_wqhd = {
 	.dsc_en = true,
 	.dsc_slice_cnt = 2,
 	.dsc_slice_height = 40,
@@ -164,13 +209,32 @@ const struct exynos_panel_desc samsung_s6e3hc2 = {
 	.max_luminance = 5400000,
 	.max_avg_luminance = 1200000,
 	.min_luminance = 5,
-	.mode = &s6e3hc2_mode,
+	.mode = &s6e3hc2_wqhd_mode,
+	.panel_func = &s6e3hc2_drm_funcs,
+	.exynos_panel_func = &s6e3hc2_exynos_funcs,
+};
+
+const struct exynos_panel_desc samsung_s6e3hc2_fhd = {
+	.dsc_en = true,
+	.dsc_slice_cnt = 2,
+	.dsc_slice_height = 65,
+	.data_lane_cnt = 4,
+	.max_brightness = 1023,
+	.dft_brightness = 511,
+	.mode_flags = MIPI_DSI_CLOCK_NON_CONTINUOUS,
+	/* supported HDR format bitmask : 1(DOLBY_VISION), 2(HDR10), 3(HLG) */
+	.hdr_formats = BIT(2),
+	.max_luminance = 5400000,
+	.max_avg_luminance = 1200000,
+	.min_luminance = 5,
+	.mode = &s6e3hc2_fhd_mode,
 	.panel_func = &s6e3hc2_drm_funcs,
 	.exynos_panel_func = &s6e3hc2_exynos_funcs,
 };
 
 static const struct of_device_id exynos_panel_of_match[] = {
-	{ .compatible = "samsung,s6e3hc2", .data = &samsung_s6e3hc2 },
+	{ .compatible = "samsung,s6e3hc2", .data = &samsung_s6e3hc2_wqhd },
+	{ .compatible = "samsung,s6e3hc2-fhd", .data = &samsung_s6e3hc2_fhd },
 	{ }
 };
 MODULE_DEVICE_TABLE(of, exynos_panel_of_match);
