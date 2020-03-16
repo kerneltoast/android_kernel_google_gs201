43d7567138dee408c0c48072c0229ba46c02e08a
Hyunki Koo <hyunki00.koo@samsung.com>
2020-03-16 14:14:11 +0900

    pinctrl: samsung: add pinctrl-gs
    Bug: 163830440
    Signed-off-by: Hyunki Koo <hyunki00.koo@samsung.com>
    Change-Id: Id84060c1ae856c0a95a8d9fd7a68fa006763ff5d
    Signed-off-by: J. Avila <elavila@google.com>

--- drivers/pinctrl/samsung/pinctrl-samsung.c
+++ drivers/pinctrl/samsung/pinctrl-samsung.c
@@ -1265,6 +1265,8 @@ static const struct of_device_id samsung_pinctrl_dt_match[] = {
 		.data = &exynos5433_of_data },
 	{ .compatible = "samsung,exynos7-pinctrl",
 		.data = &exynos7_of_data },
+	{ .compatible = "google,gs101-pinctrl",
+		.data = &gs101_of_data },
 #endif
 #ifdef CONFIG_PINCTRL_S3C64XX
 	{ .compatible = "samsung,s3c64xx-pinctrl",
