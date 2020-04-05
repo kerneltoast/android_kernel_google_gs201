41409cf11bfab46119b5ce1661e02a03f7c5f287
Sultan Alsawaf <sultan@kerneltoast.com>
2020-04-04 17:48:40 -0700

    mm: vmpressure: Ignore allocation orders above PAGE_ALLOC_COSTLY_ORDER
    PAGE_ALLOC_COSTLY_ORDER allocations can cause vmpressure to incorrectly
    think that memory pressure is high, when it's really just that the
    allocation's high order is difficult to satisfy. When this rare scenario
    occurs, ignore the input to vmpressure to avoid sending out a spurious
    high-pressure signal.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -5468,7 +5468,7 @@ static int shrink_one(struct lruvec *lruvec, struct scan_control *sc)
 
 	if (!sc->proactive)
 		vmpressure(sc->gfp_mask, memcg, false, sc->nr_scanned - scanned,
-			   sc->nr_reclaimed - reclaimed);
+			   sc->nr_reclaimed - reclaimed, sc->order);
 
 	sc->nr_reclaimed += current->reclaim_state->reclaimed_slab;
 	current->reclaim_state->reclaimed_slab = 0;
@@ -6591,7 +6591,7 @@ static void shrink_node_memcgs(pg_data_t *pgdat, struct scan_control *sc)
 		if (!sc->proactive)
 			vmpressure(sc->gfp_mask, memcg, false,
 				   sc->nr_scanned - scanned,
-				   sc->nr_reclaimed - reclaimed);
+				   sc->nr_reclaimed - reclaimed, sc->order);
 
 	} while ((memcg = mem_cgroup_iter(target_memcg, memcg, NULL)));
 }
@@ -6629,7 +6629,7 @@ again:
 	if (!sc->proactive)
 		vmpressure(sc->gfp_mask, sc->target_mem_cgroup, true,
 			   sc->nr_scanned - nr_scanned,
-			   sc->nr_reclaimed - nr_reclaimed);
+			   sc->nr_reclaimed - nr_reclaimed, sc->order);
 
 	if (sc->nr_reclaimed - nr_reclaimed)
 		reclaimable = true;
