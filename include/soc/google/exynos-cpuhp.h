8a49903a143c9a2e7b4b356d9c3963c78561667b
Hyunji Hong <hyunji.hong@samsung.corp-partner.google.com>
2020-06-03 14:31:47 +0900

    soc: google: Added exynos-cpuhp driver for module
    - Update exynos-cpuhp driver for 5.4 kernel
    
    Bug: 163150513
    Signed-off-by: Hyunji Hong <hyunji.hong@samsung.corp-partner.google.com>
    Change-Id: I93c543b833a87dee59f98e49be673e26a7598bbb
    [elavila: ported to mainline]
    Signed-off-by: J. Avila <elavila@google.com>

---
+++ include/soc/google/exynos-cpuhp.h
@@ -0,0 +1,26 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2020 Samsung Electronics Co., Ltd.
+ *		http://www.samsung.com/
+ *
+ * GS101 SoC CPU Hotplug CONTROL support
+ *
+ */
+
+#ifndef __EXYNOS_CPU_HP_H
+#define __EXYNOS_CPU_HP_H __FILE__
+
+#if IS_ENABLED(CONFIG_EXYNOS_CPUHP)
+extern int exynos_cpuhp_unregister(char *name, struct cpumask mask);
+extern int exynos_cpuhp_register(char *name, struct cpumask mask);
+extern int exynos_cpuhp_request(char *name, struct cpumask mask);
+#else
+static inline int exynos_cpuhp_unregister(char *name, struct cpumask mask)
+{ return -1; }
+static inline int exynos_cpuhp_register(char *name, struct cpumask mask)
+{ return -1; }
+static inline int exynos_cpuhp_request(char *name, struct cpumask mask)
+{ return -1; }
+#endif
+
+#endif /* __EXYNOS_CPU_HP_H */
