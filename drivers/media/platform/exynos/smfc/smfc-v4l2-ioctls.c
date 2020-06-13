d9bcb67dad719614a416b69b9acfbf2cbe1c1732
Cho KyongHo <pullip.cho@samsung.com>
2020-06-13 21:47:21 +0900

    media: smfc: add missing SPDX License Identifiers
    Signed-off-by: Cho KyongHo <pullip.cho@samsung.com>
    Change-Id: I0b55fd8430e49168e452aafcc68f06661dd011a8
    (cherry picked from commit 63792ac21de6ccae2b66d1b536aa54754f3b6a23)
    Signed-off-by: Giuliano Procida <gprocida@google.com>

--- drivers/media/platform/exynos/smfc/smfc-v4l2-ioctls.c
+++ drivers/media/platform/exynos/smfc/smfc-v4l2-ioctls.c
@@ -1,12 +1,9 @@
+// SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2015 Samsung Electronics Co., Ltd.
  *		http://www.samsung.com
  *
  * The main source file of Samsung Exynos SMFC Driver
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
  */
 
 #include <linux/kernel.h>
