70523b5c6312f1375de7a82a28c5edddd55cb7fa
Jiun Yu <jiun.yu@samsung.com>
2020-06-22 08:13:42 +0900

    drm: samsung: panel: use drm_probe_helper.h
    The drm_helper_probe_single_connector_modes() is moved to
    drm_probe_helper.h
    
    Signed-off-by: Jiun Yu <jiun.yu@samsung.com>
    Change-Id: I852f85ba4c51ad1c892b0f467b5b07be700cf01d
    Signed-off-by: Will McVicker <willmcvicker@google.com>

--- drivers/gpu/drm/panel/panel-samsung-drv.c
+++ drivers/gpu/drm/panel/panel-samsung-drv.c
@@ -21,7 +21,7 @@
 #include <drm/drm_encoder.h>
 #include <drm/drm_atomic.h>
 #include <drm/drm_atomic_helper.h>
-#include <drm/drm_crtc_helper.h>
+#include <drm/drm_probe_helper.h>
 
 #include <panel-samsung-drv.h>
 
