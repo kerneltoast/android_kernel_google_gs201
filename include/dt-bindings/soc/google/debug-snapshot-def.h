ee17c588fa4d8ced118bbc522663d7eb09b1afae
Changki Kim <changki.kim@samsung.com>
2020-08-02 10:00:33 +0900

    dt-bindings: google: Add size and address definition of debug snapshot
    Signed-off-by: Changki Kim <changki.kim@samsung.com>
    Change-Id: I071df100b445ddb81d11631b3a5019fcf7836c65
    (cherry picked from commit 49f64f5865582069ca48872a091332281dd89cf6)
    Signed-off-by: J. Avila <elavila@google.com>

---
+++ include/dt-bindings/soc/google/debug-snapshot-def.h
@@ -0,0 +1,74 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+#ifndef DEBUG_SNAPSHOT_DEF_H
+#define DEBUG_SNAPSHOT_DEF_H
+
+#define SZ_64K			0x00010000
+#define SZ_1M			0x00100000
+
+#define DSS_START_ADDR		0xD8100000
+#define DSS_HEADER_SIZE		SZ_64K
+#define DSS_LOG_KEVENTS_SIZE	(7 * SZ_1M)
+#define DSS_LOG_S2D_SIZE	(21 * SZ_1M)
+#define DSS_LOG_ARRAYRESET_SIZE	(10 * SZ_1M)
+#define DSS_LOG_ARRAYPANIC_SIZE	(10 * SZ_1M)
+
+#define DSS_HEADER_OFFSET	0
+#define DSS_HEADER_ADDR		(DSS_START_ADDR + DSS_HEADER_OFFSET)
+#define DSS_LOG_KEVENTS_ADDR	(DSS_HEADER_ADDR + DSS_HEADER_SIZE)
+#define DSS_LOG_S2D_ADDR	(DSS_LOG_KEVENTS_ADDR + DSS_LOG_KEVENTS_SIZE)
+#define DSS_LOG_ARRAYRESET_ADDR	(DSS_LOG_S2D_ADDR + DSS_LOG_S2D_SIZE)
+#define DSS_LOG_ARRAYPANIC_ADDR	(DSS_LOG_ARRAYRESET_ADDR + DSS_LOG_ARRAYRESET_SIZE)
+
+/* KEVENT ID */
+#define DSS_ITEM_HEADER		"header"
+#define DSS_ITEM_KEVENTS	"log_kevents"
+#define DSS_ITEM_S2D		"log_s2d"
+#define DSS_ITEM_ARRDUMP_RESET	"log_array_reset"
+#define DSS_ITEM_ARRDUMP_PANIC	"log_array_panic"
+
+#define DSS_LOG_TASK		"task_log"
+#define DSS_LOG_WORK		"work_log"
+#define DSS_LOG_CPUIDLE		"cpuidle_log"
+#define DSS_LOG_SUSPEND		"suspend_log"
+#define DSS_LOG_IRQ		"irq_log"
+#define DSS_LOG_HRTIMER		"hrtimer_log"
+#define DSS_LOG_CLK		"clk_log"
+#define DSS_LOG_PMU		"pmu_log"
+#define DSS_LOG_FREQ		"freq_log"
+#define DSS_LOG_DM		"dm_log"
+#define DSS_LOG_REGULATOR	"regulator_log"
+#define DSS_LOG_THERMAL		"thermal_log"
+#define DSS_LOG_ACPM		"acpm_log"
+#define DSS_LOG_PRINTK		"printk_log"
+
+/* MODE */
+#define NONE_DUMP		0
+#define FULL_DUMP		1
+#define QUICK_DUMP		2
+
+/* ACTION */
+#define GO_DEFAULT		"default"
+#define GO_DEFAULT_ID		0
+#define GO_PANIC		"panic"
+#define GO_PANIC_ID		1
+#define GO_WATCHDOG		"watchdog"
+#define GO_WATCHDOG_ID		2
+#define GO_S2D			"s2d"
+#define GO_S2D_ID		3
+#define GO_ARRAYDUMP		"arraydump"
+#define GO_ARRAYDUMP_ID		4
+#define GO_SCANDUMP		"scandump"
+#define GO_SCANDUMP_ID		5
+
+/* EXCEPTION POLICY */
+#define DPM_F			"feature"
+#define DPM_P			"policy"
+
+#define DPM_P_EL1_DA		"el1_da"
+#define DPM_P_EL1_IA		"el1_ia"
+#define DPM_P_EL1_UNDEF		"el1_undef"
+#define DPM_P_EL1_SP_PC		"el1_sp_pc"
+#define DPM_P_EL1_INV		"el1_inv"
+#define DPM_P_EL1_SERROR	"el1_serror"
+#endif
