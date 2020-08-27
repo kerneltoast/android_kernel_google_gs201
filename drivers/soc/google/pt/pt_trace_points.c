1839e70434912c48625f31d164280d38827dd06c
Thierry Strudel <tstrudel@google.com>
2020-08-27 14:53:37 -0700

    drivers: pt: make a slc_pt module with all the needed objects
    Bug: 166635685
    Signed-off-by: Thierry Strudel <tstrudel@google.com>
    Change-Id: I0cd71f54c739c605312ecc69d39615d4749eb921
    (cherry picked from commit d20355a8a654a4c0f4e1cf5d991391c6194f0849)
    Signed-off-by: Mark Salyzyn <salyzyn@google.com>
    Bug: 167676833

--- drivers/soc/google/pt/pt_trace_points.c
+++ drivers/soc/google/pt/pt_trace_points.c
@@ -13,7 +13,3 @@
 
 #define CREATE_TRACE_POINTS
 #include "pt_trace.h"
-
-EXPORT_SYMBOL_GPL(__tracepoint_pt_driver_log);
-EXPORT_SYMBOL_GPL(__tracepoint_pt_resize_callback);
-EXPORT_SYMBOL_GPL(__tracepoint_pt_enable);
