5aa0bcc227896ade639bbabf76394706ffcbe677
J. Avila <elavila@google.com>
2020-09-02 20:13:27 +0000

    include: add kernel 5.4 DT bindings for CPIF
    Add DT bindings needed for 5.4 kernel CPIF.
    
    Signed-off-by: Namkyu Kim <namkyu78.kim@samsung.com>
    Change-Id: I81d2a90b69f7bbbba06b07d36a6847ab6248f0d9
    (cherry picked from commit 72b2f22634bda71841000f34c6b09e3ea9f4cecd)
    [elavila: split DT bindings and other includes]
    Signed-off-by: J. Avila <elavila@google.com>

---
+++ include/dt-bindings/soc/google/exynos-dit.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2020 Samsung Electronics Co., Ltd.
+ *
+ * Device Tree binding constants for Exynos DIT
+ */
+
+#ifndef _DT_BINDINGS_EXYNOS_DIT_H
+#define _DT_BINDINGS_EXYNOS_DIT_H
+
+/* capabilities */
+#define DIT_CAP_MASK_NONE		(0x0)
+#define DIT_CAP_MASK_PORT_BIG_ENDIAN	(0x1 << 0)	/* deprecated */
+
+#endif /* _DT_BINDINGS_EXYNOS_DIT_H */
