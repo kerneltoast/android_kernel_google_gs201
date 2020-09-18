d1977455f8ae8166282b74760863118487766357
Xinhui Zhou <zxinhui@google.com>
2020-09-18 13:41:55 -0700

    dt-bindings: audio: add binding file for alsa
    Add the binding file for ALSA front and back ends
    
    Bug: 163009257
    
    Signed-off-by: Xinhui Zhou <zxinhui@google.com>
    Change-Id: Id2d5ed3ce92a2b3b9f26ca42140de1e8d2fe6ceb

---
+++ include/dt-bindings/sound/google-aoc.h
@@ -0,0 +1,33 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __DT_GOOGLE_AOC_H
+#define __DT_GOOGLE_AOC_H
+
+#define I2S_0_RX 0x80000000
+#define I2S_0_TX 0xC0000001
+#define I2S_1_RX 0x80000002
+#define I2S_1_TX 0xC0000003
+#define I2S_2_RX 0x80000004
+#define I2S_2_TX 0xC0000005
+#define TDM_0_RX 0x80000006
+#define TDM_0_TX 0xC0000007
+#define TDM_1_RX 0x80000008
+#define TDM_1_TX 0xC0000009
+
+#define IDX_EP1_RX	0x0
+#define IDX_EP2_RX	0x1
+#define IDX_EP3_RX	0x2
+#define IDX_EP4_RX	0x3
+#define IDX_EP5_RX	0x4
+#define IDX_EP6_RX	0x5
+#define IDX_EP7_RX	0x6
+#define IDX_EP8_RX	0x7
+
+#define IDX_EP1_TX	0x40000000
+#define IDX_EP2_TX	0x40000001
+#define IDX_EP3_TX	0x40000002
+#define IDX_EP4_TX	0x40000003
+#define IDX_EP5_TX	0x40000004
+#define IDX_EP6_TX	0x40000005
+#define IDX_EP7_TX	0x40000006
+#define IDX_EP8_TX	0x40000007
+#endif /* __DT_GOOGLE_AOC_H */
