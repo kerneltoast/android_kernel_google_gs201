186e82a968b2b3e74eaf05b770c2472d903caf02
Jinkyu Yang <jinkyu1.yang@samsung.com>
2020-10-05 20:26:15 +0900

    GKI: media: mfc: VFL_TYPE_GRABBER replaced with VFL_TYPE_VIDEO for DRV 6.0
    VFL_TYPE_GRABBER was renamed to be more meaningful.
    This patch refer below commit.
    
    8e9395b GKI: media: mfc: VFL_TYPE_GRABBER replaced with VFL_TYPE_VIDEO
    
    Change-Id: I4acba155e352b7cf7a439ded0166403551ca2aa3
    Signed-off-by: Mark Salyzyn <salyzyn@google.com>
    Signed-off-by: Jinkyu Yang <jinkyu1.yang@samsung.com>

--- drivers/media/platform/exynos/mfc/mfc.c
+++ drivers/media/platform/exynos/mfc/mfc.c
@@ -943,7 +943,7 @@ static struct video_device *__mfc_video_device_register(struct mfc_dev *dev,
 
 	snprintf(vfd->name, sizeof(vfd->name), "%s", vfd->name);
 
-	ret = video_register_device(vfd, VFL_TYPE_GRABBER, node_num);
+	ret = video_register_device(vfd, VFL_TYPE_VIDEO, node_num);
 	if (ret) {
 		v4l2_err(&dev->v4l2_dev, "Failed to register video device /dev/video%d\n", node_num);
 		video_device_release(vfd);
