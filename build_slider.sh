42207819937e31e3ab7a5ce1a7a0cfc7334dcab9
Petri Gynther <pgynther@google.com>
2020-12-10 16:55:01 -0800

    build_slider.sh: enable -Werror
    Bug: 175212278
    Signed-off-by: Petri Gynther <pgynther@google.com>
    Change-Id: I8aaabfa2a0e4f088063464e8c0da28ac01a6601a

--- build_slider.sh
+++ build_slider.sh
@@ -21,7 +21,7 @@ OUT_DIR=${BASE_OUT}/android12-5.10-staging/
 # Now build the GKI kernel
 SKIP_CP_KERNEL_HDR=1 \
   BUILD_CONFIG=common/build.config.gki.aarch64 \
-  build/build.sh "$@"
+  build/build.sh KCFLAGS=-Werror "$@"
 error_code=$?
 if [ $error_code -ne 0 ]; then
   echo "ERROR: Failed to compile android12-5.10-staging. (ret=$error_code)" >&2
@@ -31,5 +31,5 @@ fi
 OUT_DIR=${BASE_OUT}/device-kernel/
 # build the whitefin/slider Kernel
 BUILD_CONFIG=private/gs-google/build.config.slider \
-  build/build.sh "$@"
+  build/build.sh KCFLAGS=-Werror "$@"
 
