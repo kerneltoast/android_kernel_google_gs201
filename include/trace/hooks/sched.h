fb397dc77a438ba6a582e240184b5e48d4224eea
Quentin Perret <qperret@google.com>
2021-01-22 12:11:08 +0000

    Revert "ANDROID: sched: Add vendor hook for map_util_freq"
    This reverts commit c784bb6072698a9828673d963280ef75a6a9bcdc.
    
    Signed-off-by: Quentin Perret <qperret@google.com>
    Change-Id: I89d2476dd28e27453ccd7a1e3e963c270fdf2838

--- include/trace/hooks/sched.h
+++ include/trace/hooks/sched.h
@@ -92,11 +92,6 @@ DECLARE_RESTRICTED_HOOK(android_rvh_set_sugov_update,
 DECLARE_RESTRICTED_HOOK(android_rvh_cpu_overutilized,
 	TP_PROTO(int cpu, int *overutilized),
 	TP_ARGS(cpu, overutilized), 1);
-
-DECLARE_RESTRICTED_HOOK(android_rvh_map_util_freq,
-	TP_PROTO(unsigned long util, unsigned long freq, unsigned long cap,
-		 unsigned long *mapped_freq),
-	TP_ARGS(util, freq, cap, mapped_freq), 1);
 #else
 #define trace_android_rvh_select_task_rq_fair(p, prev_cpu, sd_flag, wake_flags, new_cpu)
 #define trace_android_rvh_select_task_rq_rt(p, prev_cpu, sd_flag, wake_flags, new_cpu)
@@ -117,7 +112,6 @@ DECLARE_RESTRICTED_HOOK(android_rvh_map_util_freq,
 #define trace_android_rvh_set_iowait(p, should_iowait_boost)
 #define trace_android_rvh_set_sugov_update(sg_policy, next_freq, should_update)
 #define trace_android_rvh_cpu_overutilized(cpu, overutilized)
-#define trace_android_rvh_map_util_freq(util, freq, cap, mapped_freq)
 #endif
 #endif /* _TRACE_HOOK_SCHED_H */
 /* This part must be outside protection */
