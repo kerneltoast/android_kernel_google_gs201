4c9a9c33a06134aea1d1109fd072883a7a6996cd
Quentin Perret <qperret@google.com>
2021-01-22 12:18:19 +0000

    ANDROID: Partial revert of 03177ef82bd9 ("ANDROID: sched: Add vendor hooks for override sugov behavior")
    Remove the set_sugov_sched_attr hook which is no longer needed with a
    modular governor. The IOWait hook must stay, however.
    
    Bug: 171598214
    Signed-off-by: Quentin Perret <qperret@google.com>
    Change-Id: I47cc85ca87752146608b1d73463f999ea9974a74

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -63,7 +63,6 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_gic_resume);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_wq_lockup_pool);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ipi_stop);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_find_energy_efficient_cpu);
-EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_set_sugov_sched_attr);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_set_iowait);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_cpu_overutilized);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_pstore_console_mkfile);
