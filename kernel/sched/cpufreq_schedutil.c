1f48b9f99a91d76283da01b786ae1fdc0a52f29f
Quentin Perret <qperret@google.com>
2021-01-22 12:47:39 +0000

    Revert "ANDROID: sched: cpufreq_schedutil: add trace to check uclamp effect"
    This reverts commit 79bcda576d26c7c92ae1326aef03e8a1b78fb3bd.
    
    Signed-off-by: Quentin Perret <qperret@google.com>
    Change-Id: Id92d06352fe18e5f3bd98b64bc80dcf0f3186066

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -238,10 +238,8 @@ unsigned long schedutil_cpu_util(int cpu, unsigned long util_cfs,
 	 * frequency will be gracefully reduced with the utilization decay.
 	 */
 	util = util_cfs + cpu_util_rt(rq);
-	if (type == FREQUENCY_UTIL) {
+	if (type == FREQUENCY_UTIL)
 		util = uclamp_rq_util_with(rq, util, p);
-		trace_schedutil_cpu_util_clamp_tp(cpu, util_cfs, cpu_util_rt(rq), util, max);
-	}
 
 	dl_util = cpu_util_dl(rq);
 
