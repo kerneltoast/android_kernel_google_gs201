2a5f3d78a3beed8a0c05495d3e9d646d765cfcd1
Quentin Perret <qperret@google.com>
2021-01-22 12:14:30 +0000

    Revert "ANDROID: sched: Add vendor hooks for skipping sugov update"
    This reverts commit f9f3464532a045257f8138338b1beda86ef0a3be.
    
    Change-Id: Id7507730c4e2b55334cc9d769a784191214764fc
    Signed-off-by: Quentin Perret <qperret@google.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -65,7 +65,6 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ipi_stop);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_find_energy_efficient_cpu);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_set_sugov_sched_attr);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_set_iowait);
-EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_set_sugov_update);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_cpu_overutilized);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_pstore_console_mkfile);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_pstore_console_read);
