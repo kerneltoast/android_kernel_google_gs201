702ae86007fdaedf8814d134a1d6c0048c23a537
taehyun cho <taehyun.cho@samsung.com>
2021-01-30 10:22:05 +0900

    usb: dwc3: add exynos otg driver
    Dwc3 exynos OTG driver takes care of main functionality of
    dual role between usb gadget and host mode. IF-PMIC driver
    notifies vbus and id detection and Exynos OTG driver enable
    usb gadget or host mode depending on which usb mode is detected.
    
    Bug: 177879761
    Change-Id: Ie6120f8a51cc1bf32b722cf84faec6801608efe6
    Signed-off-by: taehyun cho <taehyun.cho@samsung.com>

--- drivers/usb/dwc3/dwc3-exynos-ldo.h
+++ drivers/usb/dwc3/dwc3-exynos-ldo.h
@@ -1,4 +1,11 @@
 /* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * exynos-otg.h - Samsung EXYNOS OTG Header
+ *
+ * Copyright (c) 2021 Samsung Electronics Co., Ltd.
+ *		http://www.samsung.com
+ *
+ */
 
 #ifndef __LINUX_USB_DWC3_EXYNOS_LDO_H
 #define __LINUX_USB_DWC3_EXYNOS_LDO_H
