fa63610121f851c51aa6f8b0ec454498f540afe5
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2021-03-26 16:29:32 +0100

    locking/rtmutex: Remove output from deadlock detector
    The rtmutex specific deadlock detector predates lockdep coverage of rtmutex
    and since commit f5694788ad8da ("rt_mutex: Add lockdep annotations") it
    contains a lot of redundant functionality:
    
     - lockdep will detect an potential deadlock before rtmutex-debug
       has a chance to do so
    
     - the deadlock debugging is restricted to rtmutexes which are not
       associated to futexes and have an active waiter, which is covered by
       lockdep already
    
    Remove the redundant functionality and move actual deadlock WARN() into the
    deadlock code path. The latter needs a seperate cleanup.
    
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Acked-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lore.kernel.org/r/20210326153943.320398604@linutronix.de

--- kernel/locking/rtmutex.c
+++ kernel/locking/rtmutex.c
@@ -580,7 +580,6 @@ static int rt_mutex_adjust_prio_chain(struct task_struct *task,
 	 * walk, we detected a deadlock.
 	 */
 	if (lock == orig_lock || rt_mutex_owner(lock) == top_task) {
-		debug_rt_mutex_deadlock(chwalk, orig_waiter, lock);
 		raw_spin_unlock(&lock->wait_lock);
 		ret = -EDEADLK;
 		goto out_unlock_pi;
@@ -1173,8 +1172,6 @@ __rt_mutex_slowlock(struct rt_mutex *lock, int state,
 
 		raw_spin_unlock_irq(&lock->wait_lock);
 
-		debug_rt_mutex_print_deadlock(waiter);
-
 		schedule();
 
 		raw_spin_lock_irq(&lock->wait_lock);
@@ -1199,7 +1196,7 @@ static void rt_mutex_handle_deadlock(int res, int detect_deadlock,
 	/*
 	 * Yell loudly and stop the task right here.
 	 */
-	rt_mutex_print_deadlock(w);
+	WARN(1, "rtmutex deadlock detected\n");
 	while (1) {
 		set_current_state(TASK_INTERRUPTIBLE);
 		schedule();
@@ -1706,8 +1703,6 @@ int __rt_mutex_start_proxy_lock(struct rt_mutex *lock,
 		 */
 		ret = 0;
 	}
-
-	debug_rt_mutex_print_deadlock(waiter);
 
 	return ret;
 }
