648e992dbdd2c5c9bf42f77af5e4a1dc53d9fded
raj.varma <raj.varma@samsung.com>
2021-04-12 23:36:39 -0700

    soc: google: bcm_dbg: To dump the bcm debug data through sysfs interface
    Description:
    
    This patch replaces the bcm_dbg file writing with 'cat bcm_dump' by
    incorporating a sysfs interface.
    
    Test procedure:
    
    Go to 'adb shell' & perform the following to generate the bcm dump data:
    
    su
    cd /sys/devices/platform/gs101-ppmu/bcm_attr
    echo 1 > run_ctrl
    echo 0 > run_ctrl
    cat bcm_dump
    
    BUG: 183615064
    BUG: 179216263
    Signed-off-by: raj.varma <raj.varma@samsung.com>
    Signed-off-by: Peter Csaszar <pcsaszar@google.com>
    Change-Id: I386c6580157f9711fc875937a1fbdbc6437eaa27
    (cherry picked from commit 1675d507e6fce073284bc0f56824999501a0ea6e)

--- include/soc/google/exynos-bcm_dbg-dump.h
+++ include/soc/google/exynos-bcm_dbg-dump.h
@@ -7,20 +7,9 @@
 #define __EXYNOS_BCM_DBG_DUMP_H_
 
 /* BCM DUMP format definition */
-#define BCM_DUMP_PRE_DEFINE_SHIFT		(16)
-#define BCM_DUMP_MAX_STR			(4 * 1024)
-
-struct exynos_bcm_out_data {
-	u32				measure_time;
-	u32				ccnt;
-	u32				pmcnt[BCM_EVT_EVENT_MAX];
-};
-
-struct exynos_bcm_accumulator_data {
-	u64				measure_time;
-	u64				ccnt;
-	u64				pmcnt[BCM_EVT_EVENT_MAX];
-};
+#define BCM_DUMP_PRE_DEFINE_SHIFT	(16)
+#define BCM_DUMP_MAX_STR		(4 * 1024)
+#define BCM_DUMP_MAX_LINE_SIZE		(256)
 
 struct exynos_bcm_dump_info {
 	/*
@@ -33,10 +22,30 @@ struct exynos_bcm_dump_info {
 	u32				dump_seq_no;
 } __attribute__((packed));
 
+struct exynos_bcm_out_data {
+	u32				measure_time;
+	u32				ccnt;
+	u32				pmcnt[BCM_EVT_EVENT_MAX];
+};
+
+struct exynos_bcm_dump_entry {
+	struct exynos_bcm_dump_info	dump_info;
+	struct exynos_bcm_out_data	dump_data;
+};
+
+struct exynos_bcm_accumulator_data {
+	u64				measure_time;
+	u64				ccnt;
+	u64				pmcnt[BCM_EVT_EVENT_MAX];
+};
+
 #if IS_ENABLED(CONFIG_EXYNOS_BCM_DBG_DUMP)
 int exynos_bcm_dbg_buffer_dump(struct exynos_bcm_dbg_data *data);
+int exynos_bcm_dbg_dump(struct exynos_bcm_dbg_data *data, char *buff_out, size_t size_buff,
+		loff_t off);
 #else
 #define exynos_bcm_dbg_buffer_dump(a) do {} while (0)
+#define exynos_bcm_dbg_dump(b, c, d, e) do {} while (0)
 #endif
 
 int exynos_bcm_dbg_print_accumulators(struct exynos_bcm_dbg_data *data,
