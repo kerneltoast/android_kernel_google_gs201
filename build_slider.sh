299400da07c2dd98318bee41c6e2ead2c6f41636
Will McVicker <willmcvicker@google.com>
2021-04-13 12:50:20 -0700

    build_slider.sh: Add support to update the ABI
    Adds the flag BUILD_ABI which can be used to update the pixel ABI xml
    and symbol list. It also ensures that the symbols in the file
    `android/abi_gki_aarch64_core` are removed from the pixel symbol list.
    
    To update the ABI, run the command:
    
      $ BUILD_ABI=1 ./build_slider.sh
    
    Signed-off-by: Will McVicker <willmcvicker@google.com>
    Change-Id: I5cada6b4fa72006f8528537f84d327710d819b63

--- build_slider.sh
+++ build_slider.sh
@@ -12,13 +12,11 @@ fi
 export MIXED_BUILD=1
 
 BASE_OUT=${OUT_DIR:-out}/mixed/
-export OUT_DIR
-
-# share a common DIST_DIR
-export DIST_DIR=${DIST_DIR:-${BASE_OUT}/dist/}
-
-# build with LTO=thin by default
-export LTO=${LTO:-thin}
+BUILD_SCRIPT="build/build.sh"
+if [ -n "${BUILD_ABI}" ]; then
+  BUILD_SCRIPT="build/build_abi.sh --update"
+  export MIXED_BUILD=
+fi
 
 if [ ${LTO} == "none" ]; then
     ENABLE_STRICT_KMI=0
@@ -26,33 +24,46 @@ else
     ENABLE_STRICT_KMI=1
 fi
 
-if [ -n "${EXPERIMENTAL_BUILD}" ]; then
+if [ -z "${BUILD_ABI}" ]; then
+  if [ -n "${EXPERIMENTAL_BUILD}" ]; then
     KERNEL_OUT_DIR=android12-5.10-staging
     KERNEL_BUILD_CONFIG=common/build.config.gki.aarch64
-else
+  else
     KERNEL_OUT_DIR=android12-5.10
     KERNEL_BUILD_CONFIG=aosp/build.config.gki.aarch64
+  fi
+
+  # build with LTO=thin by default
+  export LTO=${LTO:-thin}
+
+  # Now build the GKI kernel
+  SKIP_CP_KERNEL_HDR=1 \
+    OUT_DIR=${BASE_OUT}/${KERNEL_OUT_DIR} \
+    DIST_DIR=${DIST_DIR:-${BASE_OUT}/dist/} \
+    KMI_SYMBOL_LIST_STRICT_MODE=${ENABLE_STRICT_KMI} \
+    BUILD_CONFIG=${KERNEL_BUILD_CONFIG} \
+    build/build.sh KCFLAGS=-Werror "$@"
+  error_code=$?
+  if [ $error_code -ne 0 ]; then
+    echo "ERROR: Failed to compile ${KERNEL_OUT_DIR}. (ret=$error_code)" >&2
+    exit "$error_code"
+  fi
 fi
 
-OUT_DIR=${BASE_OUT}/${KERNEL_OUT_DIR}/
-
-# Now build the GKI kernel
-SKIP_CP_KERNEL_HDR=1 \
-  KMI_SYMBOL_LIST_STRICT_MODE=${ENABLE_STRICT_KMI} \
-  BUILD_CONFIG=${KERNEL_BUILD_CONFIG} \
-  build/build.sh KCFLAGS=-Werror "$@"
-error_code=$?
-if [ $error_code -ne 0 ]; then
-  echo "ERROR: Failed to compile android12-5.10-staging. (ret=$error_code)" >&2
-  exit "$error_code"
-fi
-
-OUT_DIR=${BASE_OUT}/device-kernel/
 # build the whitefin/slider Kernel
 BUILD_CONFIG=private/gs-google/build.config.slider \
-  build/build.sh KCFLAGS=-Werror "$@"
+  OUT_DIR=${BASE_OUT}/device-kernel/ \
+  DIST_DIR=${DIST_DIR:-${BASE_OUT}/dist/} \
+  ${BUILD_SCRIPT} KCFLAGS=-Werror "$@"
 error_code=$?
 if [ $error_code -ne 0 ]; then
   echo "ERROR: Failed to compile device-kernel. (ret=$error_code)" >&2
   exit "$error_code"
+fi
+
+if [ -n "${BUILD_ABI}" ]; then
+  # Strip the core ABI symbols from the pixel symbol list
+  grep "^ " private/gs-google/android/abi_gki_aarch64_core | while read l; do
+    sed -i "/\<$l\>/d" private/gs-google/android/abi_gki_aarch64_generic
+  done
 fi
