a67dd5c33df6a6f68c7a1cc3bfd0d6c54fa15109
Will Deacon <willdeacon@google.com>
2021-04-21 13:35:27 +0100

    clocksource: exynos_mct: Limit usage only to broadcast device on CPU 0
    All arm64 SoCs have an architected timer exposed via a per-cpu system
    register interface which is typically low-latency and implemented as
    part of the CPU.
    
    The 'exynos_mct' driver deliberately sets its clockevents rating field
    to override the architected timer, as this was apparently preferred for
    an old 32-bit SoC with shared hardware blocks for implementing the
    counter.
    
    Unfortunately, the 'exynos_mct' is relatively slow to access, so reverse
    the decision made in 6282edb72bed5 ("clocksource/drivers/exynos_mct:
    Increase priority over ARM arch timer") and prefer the architected timer
    instead. Since the architected timer cannot be used to wake up from
    cpuidle states, the MCT on CPU 0 is used as a broadcast device to wake
    others from idle.
    
    Bug: 185092876
    Fixes: 6282edb72bed5 ("clocksource/drivers/exynos_mct: Increase priority over ARM arch timer")
    Change-Id: I4e838d14b9204eed095ab260fa41e5048cf7c9b1
    Signed-off-by: Will Deacon <willdeacon@google.com>

--- drivers/clocksource/exynos_mct.c
+++ drivers/clocksource/exynos_mct.c
@@ -206,7 +206,7 @@ static void exynos4_frc_resume(struct clocksource *cs)
 
 static struct clocksource mct_frc = {
 	.name		= "mct-frc",
-	.rating		= 450,	/* use value higher than ARM arch timer */
+	.rating		= 200,
 	.read		= exynos4_frc_read,
 	.mask		= CLOCKSOURCE_MASK(32),
 	.flags		= CLOCK_SOURCE_IS_CONTINUOUS,
@@ -454,7 +454,7 @@ static int exynos4_mct_starting_cpu(unsigned int cpu)
 	evt->set_state_oneshot_stopped = set_state_shutdown;
 	evt->tick_resume = set_state_resume;
 	evt->features = CLOCK_EVT_FEAT_PERIODIC | CLOCK_EVT_FEAT_ONESHOT;
-	evt->rating = 500;	/* use value higher than ARM arch timer */
+	evt->rating = 250;
 
 	exynos4_mct_write(TICK_BASE_CNT, mevt->base + MCT_L_TCNTB_OFFSET);
 
