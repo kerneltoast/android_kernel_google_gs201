76d061e02168020008ce438bfd6f291917c669ad
Namkyu Kim <namkyu78.kim@samsung.com>
2021-05-06 21:05:03 +0900

    soc/google/cpif: separate databuf dump into DL and UL
    While reducing the reserved memory of Pktproc DL and UL,
    the base address is the same, but the size is reduced,
    so it has to be separated.
    
    Signed-off-by: Namkyu Kim <namkyu78.kim@samsung.com>
    Change-Id: I8ed36df2502dc7da4f73fd97823fdcc6db612bec

--- drivers/soc/google/cpif/modem_dump.c
+++ drivers/soc/google/cpif/modem_dump.c
@@ -118,15 +118,17 @@ int cp_get_log_dump(struct io_device *iod, struct link_device *ld, unsigned long
 		break;
 #endif
 
-	case LOG_IDX_DATABUF:
+	case LOG_IDX_DATABUF_DL:
 		base = phys_to_virt(cp_shmem_get_base(cp_num, SHMEM_PKTPROC));
-#if IS_ENABLED(CONFIG_CP_PKTPROC_UL)
-		size = cp_shmem_get_size(cp_num, SHMEM_PKTPROC) +
-			cp_shmem_get_size(cp_num, SHMEM_PKTPROC_UL);
-#else
 		size = cp_shmem_get_size(cp_num, SHMEM_PKTPROC);
-#endif
 		break;
+
+#if IS_ENABLED(CONFIG_CP_PKTPROC_UL)
+	case LOG_IDX_DATABUF_UL:
+		base = phys_to_virt(cp_shmem_get_base(cp_num, SHMEM_PKTPROC_UL));
+		size = cp_shmem_get_size(cp_num, SHMEM_PKTPROC_UL);
+		break;
+#endif
 
 	case LOG_IDX_L2B:
 		base = phys_to_virt(cp_shmem_get_base(cp_num, SHMEM_L2B));
