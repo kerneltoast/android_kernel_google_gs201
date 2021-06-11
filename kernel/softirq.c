f9bc0da1dbc1ce1af0d0fea87c926f27f76dcdcf
Peter Zijlstra <peterz@infradead.org>
2021-06-11 10:28:11 +0200

    sched: Unbreak wakeups
    Remove broken task->state references and let wake_up_process() DTRT.
    
    The anti-pattern in these patches breaks the ordering of ->state vs
    COND as described in the comment near set_current_state() and can lead
    to missed wakeups:
    
    	(OoO load, observes RUNNING)<-.
    	for (;;) {                    |
    	  t->state = UNINTERRUPTIBLE; |
    	  smp_mb();          ,----->  | (observes !COND)
                                 |        /
    	  if (COND) ---------'       |	COND = 1;
    		break;		     `- if (t->state != RUNNING)
    					  wake_up_process(t); // not done
    	  schedule(); // forever waiting
    	}
    	t->state = TASK_RUNNING;
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Davidlohr Bueso <dbueso@suse.de>
    Acked-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Acked-by: Will Deacon <will@kernel.org>
    Link: https://lore.kernel.org/r/20210611082838.160855222@infradead.org

--- kernel/softirq.c
+++ kernel/softirq.c
@@ -89,7 +89,7 @@ static void wakeup_softirqd(void)
 	/* Interrupts are disabled: no need to stop preemption */
 	struct task_struct *tsk = __this_cpu_read(ksoftirqd);
 
-	if (tsk && tsk->state != TASK_RUNNING)
+	if (tsk)
 		wake_up_process(tsk);
 }
 
