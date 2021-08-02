b3c9c7f5281cbcce7b1223fdd0b82a35c1c2d5c0
Aaron Ding <aaronding@google.com>
2021-08-02 18:50:21 +0800

    move to ACK android13-5.10
    Bug: 195268596
    Change-Id: I0b730a2104716ba71f43ae3e54f906eeada26b0f
    Signed-off-by: Aaron Ding <aaronding@google.com>

--- build_gs201_zebu.sh
+++ build_gs201_zebu.sh
@@ -18,14 +18,14 @@ export OUT_DIR
 # share a common DIST_DIR
 export DIST_DIR=${DIST_DIR:-${BASE_OUT}/dist/}
 
-OUT_DIR=${BASE_OUT}/android12-5.10-staging/
+OUT_DIR=${BASE_OUT}/android13-5.10-staging/
 # Now build the GKI kernel
 SKIP_CP_KERNEL_HDR=1 \
   BUILD_CONFIG=common/build.config.gki.aarch64 \
   build/build.sh KCFLAGS=-Werror "$@"
 error_code=$?
 if [ $error_code -ne 0 ]; then
-  echo "ERROR: Failed to compile android12-5.10-staging. (ret=$error_code)" >&2
+  echo "ERROR: Failed to compile android13-5.10-staging. (ret=$error_code)" >&2
   exit "$error_code"
 fi
 
