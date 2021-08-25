6bfd4b36a0be2f8bbac202f1fe5c21307dad810a
Albert Wang <albertccwang@google.com>
2021-08-25 13:04:25 +0000

    Revert "usb: turn LDOs back on when suspend was interrupted"
    This reverts commit 7b3259ace9848ad78127eb9c811bda1168701054.
    
    Reason for revert: caused the airplane suspend regression (b/196242282)
    
    Bug: 196242282
    Change-Id: I269858f4c65ac43c15d4a7300a7e507ae51d2822
    Signed-off-by: Albert Wang <albertccwang@google.com>

--- drivers/usb/dwc3/dwc3-exynos.c
+++ drivers/usb/dwc3/dwc3-exynos.c
@@ -1336,15 +1336,8 @@ static int dwc3_exynos_runtime_suspend(struct device *dev)
 
 	dwc = exynos->dwc;
 	spin_lock_irqsave(&dwc->lock, flags);
-	if (pm_runtime_suspended(dev)) {
-		if(!exynos_usbdrd_get_vdd_hsi_status())
-			exynos_usbdrd_vdd_hsi_manual_control(1);
-
-		if(!exynos_usbdrd_get_ldo_status())
-			exynos_usbdrd_ldo_manual_control(1);
-
+	if (pm_runtime_suspended(dev))
 		return 0;
-	}
 
 	dwc3_exynos_clk_disable(exynos);
 
@@ -1390,15 +1383,8 @@ static int dwc3_exynos_suspend(struct device *dev)
 {
 	struct dwc3_exynos *exynos = dev_get_drvdata(dev);
 
-	if (pm_runtime_suspended(dev)) {
-		if(!exynos_usbdrd_get_vdd_hsi_status())
-			exynos_usbdrd_vdd_hsi_manual_control(1);
-
-		if(!exynos_usbdrd_get_ldo_status())
-			exynos_usbdrd_ldo_manual_control(1);
-
+	if (pm_runtime_suspended(dev))
 		return 0;
-	}
 
 	dwc3_exynos_clk_disable(exynos);
 
