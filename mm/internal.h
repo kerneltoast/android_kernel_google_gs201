163cfc4f026f24e08faece2d9e60162b0f62cf6a
Sultan Alsawaf <sultan@kerneltoast.com>
2021-09-26 00:29:51 -0700

    mm: Move slab shrinkers into a dedicated thread called kshrinkd
    Slab shrinkers are unique in that they have no bearing on direct reclaim
    throttling and reclaim statistics, meaning that they can be completely
    decoupled from the kswapd and direct reclaim paths, making them faster.
    Further justification for this is that shrinkers may be slow and aren't
    guaranteed to free any memory, which means that they can waste kswapd
    and direct reclaim time without producing results. Running the same
    shrinker concurrently can also produce unwanted lock contention inside
    the shrinker itself.
    
    An empirical test of checking how long kswapd and kshrinkd ran on a
    memory constrained system showed that kshrinkd ran for about 10% of the
    duration that kswapd ran, which indicates significant savings.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- mm/internal.h
+++ mm/internal.h
@@ -395,6 +395,7 @@ extern void post_alloc_hook(struct page *page, unsigned int order,
 					gfp_t gfp_flags);
 extern int user_min_free_kbytes;
 extern atomic_long_t kswapd_waiters;
+extern atomic_long_t kshrinkd_waiters;
 
 extern void free_unref_page(struct page *page, unsigned int order);
 extern void free_unref_page_list(struct list_head *list);
