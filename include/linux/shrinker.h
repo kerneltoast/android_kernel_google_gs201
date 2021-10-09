ea443ab9400eba4c68605b20f2ddc63727eb71f8
Sultan Alsawaf <sultan@kerneltoast.com>
2021-10-09 01:12:14 -0700

    mm: Replace shrinker rwsem with fine-grained locks
    The shrinker rwsem is problematic because the actual shrinking path must
    back off when contention appears, causing some or all shrinkers to be
    skipped. This can be especially bad when shrinkers are frequently
    registered and unregistered. A high frequency of shrinker registrations/
    unregistrations can effectively DoS the shrinker mechanism, rendering it
    useless.
    
    We can't use SRCU here because it would unilaterally shift the burden
    back onto the registration/unregistration paths, which defeats the
    purpose of why the back-off mechanism was added in the first place.
    
    Using fine-grained locks and a rwsem at the per-shrinker level to
    protect the shrinker list rather than a global rwsem lets us balance the
    burden equally, eliminating the issue. Now, shrinking can happen
    concurrently with shrinker registrations/unregistrations, without either
    side being favored more than the other.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- include/linux/shrinker.h
+++ include/linux/shrinker.h
@@ -2,6 +2,8 @@
 #ifndef _LINUX_SHRINKER_H
 #define _LINUX_SHRINKER_H
 
+#include <linux/rwsem.h>
+
 /*
  * This struct is used to pass information from page reclaim to the shrinkers.
  * We consolidate the values for easier extension later.
@@ -80,6 +82,7 @@ struct shrinker {
 #endif
 	/* objs pending delete, per node */
 	atomic_long_t *nr_deferred;
+	struct rw_semaphore del_rwsem;
 };
 #define DEFAULT_SEEKS 2 /* A good number if you don't know better. */
 
