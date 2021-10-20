b110e107a61f3b1c93e818550fb49c459eed8d36
Christoph Hellwig <hch@lst.de>
2021-10-20 16:41:19 +0200

    block: cleanup the flush plug helpers
    Consolidate the various helpers into a single blk_flush_plug helper that
    takes a plk_plug and the from_scheduler bool and switch all callsites to
    call it directly.  Checks that the plug is non-NULL must be performed by
    the caller, something that most already do anyway.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Link: https://lore.kernel.org/r/20211020144119.142582-5-hch@lst.de
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    [Sultan Alsawaf: backport to 5.10]
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- block/blk-core.c
+++ block/blk-core.c
@@ -1755,7 +1755,7 @@ struct blk_plug_cb *blk_check_plugged(blk_plug_cb_fn unplug, void *data,
 }
 EXPORT_SYMBOL(blk_check_plugged);
 
-void blk_flush_plug_list(struct blk_plug *plug, bool from_schedule)
+void blk_flush_plug(struct blk_plug *plug, bool from_schedule)
 {
 	if (!list_empty(&plug->cb_list))
 		flush_plug_callbacks(plug, from_schedule);
@@ -1775,11 +1775,10 @@ void blk_flush_plug_list(struct blk_plug *plug, bool from_schedule)
  */
 void blk_finish_plug(struct blk_plug *plug)
 {
-	if (plug != current->plug)
-		return;
-	blk_flush_plug_list(plug, false);
-
-	current->plug = NULL;
+	if (plug == current->plug) {
+		blk_flush_plug(plug, false);
+		current->plug = NULL;
+	}
 }
 EXPORT_SYMBOL(blk_finish_plug);
 
