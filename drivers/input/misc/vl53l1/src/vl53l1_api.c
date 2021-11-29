5ea45fecc4e5194f946edae5074db2cb3ad23964
JimiChen <jimichen@google.com>
2021-11-29 21:16:29 +0800

    input/misc: vl53l1: add initial values
    Add initial value zero for offsetA[3] and offsetB[3] if it really
    run into the corner case like statistic code analysis.
    
    Bug: 206918720
    Test: build okay
    Signed-off-by: JimiChen <jimichen@google.com>
    Change-Id: Ie142ec81cd71befeea81089ad19115ae82c135d0

--- drivers/input/misc/vl53l1/src/vl53l1_api.c
+++ drivers/input/misc/vl53l1/src/vl53l1_api.c
@@ -3808,8 +3808,8 @@ VL53L1_Error VL53L1_PerformOffsetPerVcselCalibration(VL53L1_DEV Dev,
 	VL53L1_DistanceModes currentDist;
 	VL53L1_DistanceModes DistMode[3] = {VL53L1_DISTANCEMODE_SHORT,
 			VL53L1_DISTANCEMODE_MEDIUM, VL53L1_DISTANCEMODE_LONG};
-	int16_t offsetA[3];
-	int16_t offsetB[3];
+	int16_t offsetA[3] = {0};
+	int16_t offsetB[3] = {0};
 
 	VL53L1_Error SmudgeStatus = VL53L1_ERROR_NONE;
 	uint8_t smudge_corr_en, isc;
