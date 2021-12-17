cc1d22b6b536eb137f313ef6b6815e128ab9e35c
Thomas Gleixner <tglx@linutronix.de>
2021-12-17 11:32:09 +0100

    eventfd: Make signal recursion protection a task bit
    Upstream commit b542e383d8c005f06a131e2b40d5889b812f19c6
    
    The recursion protection for eventfd_signal() is based on a per CPU
    variable and relies on the !RT semantics of spin_lock_irqsave() for
    protecting this per CPU variable. On RT kernels spin_lock_irqsave() neither
    disables preemption nor interrupts which allows the spin lock held section
    to be preempted. If the preempting task invokes eventfd_signal() as well,
    then the recursion warning triggers.
    
    Paolo suggested to protect the per CPU variable with a local lock, but
    that's heavyweight and actually not necessary. The goal of this protection
    is to prevent the task stack from overflowing, which can be achieved with a
    per task recursion protection as well.
    
    Replace the per CPU variable with a per task bit similar to other recursion
    protection bits like task_struct::in_page_owner. This works on both !RT and
    RT kernels and removes as a side effect the extra per CPU storage.
    
    No functional change for !RT kernels.
    
    Reported-by: Daniel Bristot de Oliveira <bristot@redhat.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Tested-by: Daniel Bristot de Oliveira <bristot@redhat.com>
    Acked-by: Jason Wang <jasowang@redhat.com>
    Cc: Al Viro <viro@zeniv.linux.org.uk>
    Link: https://lore.kernel.org/r/87wnp9idso.ffs@tglx
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -926,6 +926,10 @@ struct task_struct {
 	/* Stalled due to lack of memory */
 	unsigned			in_memstall:1;
 #endif
+#ifdef CONFIG_EVENTFD
+	/* Recursion prevention for eventfd_signal() */
+	unsigned			in_eventfd_signal:1;
+#endif
 
 	unsigned long			atomic_flags; /* Flags requiring atomic access. */
 
