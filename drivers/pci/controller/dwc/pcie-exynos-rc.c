c045bdb41372f0ff4637640fb3530eb2d84d9d9a
Jonglin Lee <jonglin@google.com>
2022-02-16 18:33:38 -0800

    Revert "pcie: exynos: change code for flexible configuration"
    This reverts commit ed9d54d468c1f347b3aabd61f685c70b884bcb5f.
    
    Bug: 216612787
    Signed-off-by: Jonglin Lee <jonglin@google.com>
    Change-Id: I03820320718a764c971ea0187a56cd73f9d4c580

--- drivers/pci/controller/dwc/pcie-exynos-rc.c
+++ drivers/pci/controller/dwc/pcie-exynos-rc.c
@@ -3120,12 +3120,6 @@ static int exynos_pcie_rc_set_l1ss(int enable, struct pcie_port *pp, int id)
 			__func__, exynos_pcie->l1ss_ctrl_id_state, id);
 
 		return -1;
-	} else {
-		exynos_pcie->ep_l1ss_cap_off =
-			pci_find_ext_capability(exynos_pcie->ep_pci_dev, PCI_EXT_CAP_ID_L1SS);
-		exynos_pcie->ep_link_ctrl_off = exynos_pcie->ep_pci_dev->pcie_cap + PCI_EXP_LNKCTL;
-		exynos_pcie->ep_l1ss_ctrl1_off = exynos_pcie->ep_l1ss_cap_off + PCI_L1SS_CTL1;
-		exynos_pcie->ep_l1ss_ctrl2_off = exynos_pcie->ep_l1ss_cap_off + PCI_L1SS_CTL2;
 	}
 
 	/* get the domain_num & ep_pci_bus of EP device */
@@ -3152,9 +3146,9 @@ static int exynos_pcie_rc_set_l1ss(int enable, struct pcie_port *pp, int id)
 				exynos_pcie_rc_wr_own_conf(pp, PCIE_LINK_L1SS_CONTROL, 4, val);
 
 				/* [RC] set TPOWERON */
-				/* Set TPOWERON value for RC: 90->130 usec */
+				/* Set TPOWERON value for RC: 90->180 usec */
 				exynos_pcie_rc_wr_own_conf(pp, PCIE_LINK_L1SS_CONTROL2, 4,
-							   PORT_LINK_TPOWERON_130US);
+							   PORT_LINK_TPOWERON_180US);
 
 				/* exynos_pcie_rc_wr_own_conf(pp,
 				 *	PCIE_L1_SUBSTATES_OFF, 4,
@@ -3176,8 +3170,8 @@ static int exynos_pcie_rc_set_l1ss(int enable, struct pcie_port *pp, int id)
 				/* [EP] set TPOWERON */
 				/* Set TPOWERON value for EP: 90->180 usec */
 				exynos_pcie_rc_wr_other_conf(pp, ep_pci_bus, 0,
-							     exynos_pcie->ep_l1ss_ctrl2_off, 4,
-							     PORT_LINK_TPOWERON_130US);
+							     PCIE_LINK_L1SS_CONTROL2, 4,
+							     PORT_LINK_TPOWERON_180US);
 
 				/* [EP] set Entrance latency */
 				/* Set L1.2 Enterance Latency for EP: 64 usec */
@@ -3190,10 +3184,10 @@ static int exynos_pcie_rc_set_l1ss(int enable, struct pcie_port *pp, int id)
 
 				/* 2) [EP] enable L1SS */
 				exynos_pcie_rc_rd_other_conf(pp, ep_pci_bus, 0,
-							     exynos_pcie->ep_l1ss_ctrl1_off, 4, &val);
+							     PCIE_LINK_L1SS_CONTROL, 4, &val);
 				val |= PORT_LINK_L1SS_ENABLE;
 				exynos_pcie_rc_wr_other_conf(pp, ep_pci_bus, 0,
-							     exynos_pcie->ep_l1ss_ctrl1_off, 4, val);
+							     PCIE_LINK_L1SS_CONTROL, 4, val);
 				dev_dbg(dev, "CPen:2EP:L1SS_CTRL(0x19C)=0x%x\n", val);
 
 				/* 3) [RC] enable ASPM */
@@ -3207,11 +3201,11 @@ static int exynos_pcie_rc_set_l1ss(int enable, struct pcie_port *pp, int id)
 
 				/* 4) [EP] enable ASPM */
 				exynos_pcie_rc_rd_other_conf(pp, ep_pci_bus, 0,
-							     exynos_pcie->ep_link_ctrl_off, 4, &val);
+							     PCIE_LINK_CTRL_STAT, 4, &val);
 				val |= PCI_EXP_LNKCTL_CCC | PCI_EXP_LNKCTL_CLKREQ_EN |
 					PCI_EXP_LNKCTL_ASPM_L1;
 				exynos_pcie_rc_wr_other_conf(pp, ep_pci_bus, 0,
-							     exynos_pcie->ep_link_ctrl_off, 4, val);
+							     PCIE_LINK_CTRL_STAT, 4, val);
 				dev_dbg(dev, "CPen:4EP:ASPM(0x80)=0x%x\n", val);
 			} else if (exynos_pcie->ep_device_type == EP_BCM_WIFI ||
 				   exynos_pcie->ep_device_type == EP_QC_WIFI) {
@@ -3315,10 +3309,10 @@ static int exynos_pcie_rc_set_l1ss(int enable, struct pcie_port *pp, int id)
 
 				/* 1) [EP] disable ASPM */
 				exynos_pcie_rc_rd_other_conf(pp, ep_pci_bus, 0,
-							     exynos_pcie->ep_link_ctrl_off, 4, &val);
+							     PCIE_LINK_CTRL_STAT, 4, &val);
 				val &= ~(PCI_EXP_LNKCTL_ASPMC);
 				exynos_pcie_rc_wr_other_conf(pp, ep_pci_bus, 0,
-							     exynos_pcie->ep_link_ctrl_off, 4, val);
+							     PCIE_LINK_CTRL_STAT, 4, val);
 				dev_dbg(dev, "CPdis:1EP:ASPM(0x80)=0x%x\n", val);
 
 				/* 2) [RC] disable ASPM */
@@ -3331,10 +3325,10 @@ static int exynos_pcie_rc_set_l1ss(int enable, struct pcie_port *pp, int id)
 
 				/* 3) [EP] disable L1SS */
 				exynos_pcie_rc_rd_other_conf(pp, ep_pci_bus, 0,
-							     exynos_pcie->ep_l1ss_ctrl1_off, 4, &val);
+							     PCIE_LINK_L1SS_CONTROL, 4, &val);
 				val &= ~(PORT_LINK_L1SS_ENABLE);
 				exynos_pcie_rc_wr_other_conf(pp, ep_pci_bus, 0,
-							     exynos_pcie->ep_l1ss_ctrl1_off, 4, val);
+							     PCIE_LINK_L1SS_CONTROL, 4, val);
 				dev_dbg(dev, "CPdis:3EP:L1SS_CTRL(0x19C)=0x%x\n", val);
 
 				/* 4) [RC] disable L1SS */
