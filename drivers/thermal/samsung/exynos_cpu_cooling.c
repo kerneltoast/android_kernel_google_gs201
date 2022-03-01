e231a5e69677b6cca7e33cca3922f1bcaf649e77
Wei Wang <wvw@google.com>
2022-02-28 23:06:47 -0800

    drivers: thermal: exynos_cpu_cooling: add sysfs_node to bypass user_vote
    Bug: 221173040
    Test: Build
    Change-Id: I50a599372543260135545d0a75a046acd7be1f53
    Signed-off-by: Wei Wang <wvw@google.com>

--- drivers/thermal/samsung/exynos_cpu_cooling.c
+++ drivers/thermal/samsung/exynos_cpu_cooling.c
@@ -105,6 +105,7 @@ struct exynos_cpu_cooling_device {
 
 	struct thermal_zone_device *tzd;
 	unsigned long sysfs_req;
+	bool sysfs_req_bypass;
 };
 
 static DEFINE_IDA(cpufreq_ida);
@@ -565,7 +566,9 @@ static int cpufreq_set_cur_state(struct thermal_cooling_device *cdev,
 	if (WARN_ON(state > cpufreq_cdev->max_level))
 		return -EINVAL;
 
-	state = max(cpufreq_cdev->sysfs_req, state);
+	if (!cpufreq_cdev->sysfs_req_bypass)
+		state = max(cpufreq_cdev->sysfs_req, state);
+
 	/* Check if the old cooling action is same as new cooling action */
 	if (cpufreq_cdev->cpufreq_state == state)
 		return -EALREADY;
@@ -577,8 +580,8 @@ static int cpufreq_set_cur_state(struct thermal_cooling_device *cdev,
 
 	if (ret == 1) {
 		ret = 0;
-		trace_clock_set_rate(cdev->type, cpufreq_cdev->sysfs_req, raw_smp_processor_id());
-		trace_vendor_cdev_update(cdev->type, cpufreq_cdev->sysfs_req, state);
+		trace_clock_set_rate(cdev->type, state, raw_smp_processor_id());
+		trace_vendor_cdev_update(cdev->type, state, state);
 	}
 
 	return ret;
@@ -878,6 +881,8 @@ state2power_table_show(struct device *dev, struct device_attribute *attr, char *
 	return count;
 }
 
+static DEVICE_ATTR_RO(state2power_table);
+
 ssize_t
 user_vote_show(struct device *dev, struct device_attribute *attr, char *buf)
 {
@@ -917,7 +922,44 @@ ssize_t user_vote_store(struct device *dev, struct device_attribute *attr,
 }
 
 static DEVICE_ATTR_RW(user_vote);
-static DEVICE_ATTR_RO(state2power_table);
+
+ssize_t
+user_vote_bypass_show(struct device *dev, struct device_attribute *attr, char *buf)
+{
+	struct thermal_cooling_device *cdev = to_cooling_device(dev);
+	struct exynos_cpu_cooling_device *cpufreq_cdev = cdev->devdata;
+
+	if (!cpufreq_cdev)
+		return -ENODEV;
+
+	return sysfs_emit(buf, "%d\n", cpufreq_cdev->sysfs_req_bypass);
+}
+
+ssize_t user_vote_bypass_store(struct device *dev, struct device_attribute *attr,
+			const char *buf, size_t count)
+{
+	struct thermal_cooling_device *cdev = to_cooling_device(dev);
+	struct exynos_cpu_cooling_device *cpufreq_cdev = cdev->devdata;
+	int ret;
+	bool bypass;
+
+	if (!cpufreq_cdev)
+		return -ENODEV;
+
+	ret = kstrtobool(buf, &bypass);
+	if (ret)
+		return ret;
+
+
+	mutex_lock(&cdev->lock);
+	cpufreq_cdev->sysfs_req_bypass = bypass;
+	cdev->updated = false;
+	mutex_unlock(&cdev->lock);
+	thermal_cdev_update(cdev);
+	return count;
+}
+
+static DEVICE_ATTR_RW(user_vote_bypass);
 
 /**
  * __exynos_cpu_cooling_register - helper function to create cpufreq cooling device
@@ -1034,6 +1076,12 @@ __exynos_cpu_cooling_register(struct device_node *np,
 		goto remove_qos_req;
 
 	ret = device_create_file(&cdev->device, &dev_attr_user_vote);
+	if (ret) {
+		thermal_cooling_device_unregister(cdev);
+		goto remove_qos_req;
+	}
+
+	ret = device_create_file(&cdev->device, &dev_attr_user_vote_bypass);
 	if (ret) {
 		thermal_cooling_device_unregister(cdev);
 		goto remove_qos_req;
