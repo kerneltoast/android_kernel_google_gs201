ef2becaac44578a187b37bbd86367bf068f09bf4
Frederic Weisbecker <frederic@kernel.org>
2022-04-05 03:07:52 +0200

    tick: Fix timer storm since introduction of timersd
    If timers are pending while the tick is reprogrammed on nohz_mode, the
    next expiry is not armed to fire now, it is delayed one jiffy forward
    instead so as not to raise an inextinguishable timer storm with such
    scenario:
    
    1) IRQ triggers and queue a timer
    2) ksoftirqd() is woken up
    3) IRQ tail: timer is reprogrammed to fire now
    4) IRQ exit
    5) TIMER interrupt
    6) goto 3)
    
    ...all that until we finally reach ksoftirqd.
    
    Unfortunately we are checking the wrong softirq vector bitmask since
    timersd kthread has split from ksoftirqd. Timers now have their own
    vector state field that must be checked separately. As a result, the
    old timer storm is back. This shows up early on boot with extremely long
    initcalls:
    
    	[  333.004807] initcall dquot_init+0x0/0x111 returned 0 after 323822879 usecs
    
    and the cause is uncovered with the right trace events showing just
    10 microseconds between ticks (~100 000 Hz):
    
    |swapper/-1 1dn.h111 60818582us : hrtimer_expire_entry: hrtimer=00000000e0ef0f6b function=tick_sched_timer now=60415486608
    |swapper/-1 1dn.h111 60818592us : hrtimer_expire_entry: hrtimer=00000000e0ef0f6b function=tick_sched_timer now=60415496082
    |swapper/-1 1dn.h111 60818601us : hrtimer_expire_entry: hrtimer=00000000e0ef0f6b function=tick_sched_timer now=60415505550
    
    Fix this by checking the right timer vector state from the nohz code.
    
    Signed-off-by: Frederic Weisbecker <frederic@kernel.org>
    Cc: Mel Gorman <mgorman@suse.de>
    Cc: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Link: https://lkml.kernel.org/r/20220405010752.1347437-2-frederic@kernel.org

--- include/linux/interrupt.h
+++ include/linux/interrupt.h
@@ -587,8 +587,15 @@ extern void raise_softirq_irqoff(unsigned int nr);
 extern void raise_softirq(unsigned int nr);
 
 #ifdef CONFIG_PREEMPT_RT
+DECLARE_PER_CPU(unsigned long, pending_timer_softirq);
+
 extern void raise_timer_softirq(void);
 extern void raise_hrtimer_softirq(void);
+
+static inline unsigned int local_pending_timers(void)
+{
+        return __this_cpu_read(pending_timer_softirq);
+}
 
 #else
 static inline void raise_timer_softirq(void)
@@ -599,6 +606,11 @@ static inline void raise_timer_softirq(void)
 static inline void raise_hrtimer_softirq(void)
 {
 	raise_softirq_irqoff(HRTIMER_SOFTIRQ);
+}
+
+static inline unsigned int local_pending_timers(void)
+{
+        return local_softirq_pending();
 }
 #endif
 
