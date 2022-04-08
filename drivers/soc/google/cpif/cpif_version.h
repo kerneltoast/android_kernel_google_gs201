e9f9e7e3975fffeeb9bcbd83b1203d50643ba39a
Dongseok Yi <dseok.yi@samsung.com>
2022-04-08 09:24:17 +0900

    soc/google/cpif: update version to CPIF-20220408R1
    update version to CPIF-20220408R1
    
    Change-Id: Ib485803499bba21f836207ad6598bebaaa8710af
    Signed-off-by: Dongseok Yi <dseok.yi@samsung.com>

--- drivers/soc/google/cpif/cpif_version.h
+++ drivers/soc/google/cpif/cpif_version.h
@@ -8,5 +8,5 @@
 #define __CPIF_VERSION_H__
 
 /* Should not exceed CPIF_VERSION_SIZE */
-static const char cpif_driver_version[] = "CPIF-20220316R1";
+static const char cpif_driver_version[] = "CPIF-20220408R1";
 #endif /* __CPIF_VERSION_H__ */
