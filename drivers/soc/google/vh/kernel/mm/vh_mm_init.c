578d9d7245976b7532c4887688745acdf73d565c
Minchan Kim <minchan@google.com>
2022-04-14 23:29:22 +0000

    Revert "vendor_hook: mm: migrate CMA pages on the GUP friends"
    This reverts commit 10e2471a7ddfaf359f938da55f6899e913d981cf.
    
    Reason for revert: http://b/229290888
    
    (cherry picked from commit 261a8f4ea877e719694eed5cef5f068149bbc010)
    Change-Id: Ia9d6f26f78fac81a399d7098bdb2f4244d184158

--- drivers/soc/google/vh/kernel/mm/vh_mm_init.c
+++ drivers/soc/google/vh/kernel/mm/vh_mm_init.c
@@ -8,8 +8,6 @@
 
 #include <linux/kobject.h>
 #include <linux/module.h>
-#include <trace/hooks/gup.h>
-#include "../../include/gup.h"
 
 struct kobject *vendor_mm_kobj;
 EXPORT_SYMBOL_GPL(vendor_mm_kobj);
@@ -25,33 +23,9 @@ static int vh_mm_init(void)
 		return -ENOMEM;
 
 	ret = pixel_mm_cma_sysfs(vendor_mm_kobj);
-	if (ret) {
+	if (ret)
 		kobject_put(vendor_mm_kobj);
-		return ret;
-	}
 
-	/*
-	 * Not sure this error handling is meaningful for vendor hook.
-	 * Maybe better to rely on the just BUG_ON?
-	 */
-	ret = register_trace_android_vh_try_grab_compound_head(
-				vh_try_grab_compound_head, NULL);
-	if (ret)
-		return ret;
-	ret = register_trace_android_vh___get_user_pages_remote(
-				vh___get_user_pages_remote, NULL);
-	if (ret)
-		return ret;
-	ret = register_trace_android_vh_get_user_pages(
-				vh_android_vh_get_user_pages, NULL);
-	if (ret)
-		return ret;
-	ret = register_trace_android_vh_internal_get_user_pages_fast(
-				vh_internal_get_user_pages_fast, NULL);
-	if (ret)
-		return ret;
-	ret = register_trace_android_vh_pin_user_pages(
-				vh_pin_user_pages, NULL);
 	return ret;
 }
 module_init(vh_mm_init);
