a1f55910a6f24214ce7fc75c29798af2ec400c1f
Robin Peng <robinpeng@google.com>
2022-05-09 21:51:25 +0800

    update_symbol_list.sh: update cloudripper build files
    * update_symbol_list_cloudripper-aosp.sh for aosp kernel
    * update_symbol_list_cloudripper-staging.sh for staging kernel
    
    Bug: 230573746
    Signed-off-by: Robin Peng <robinpeng@google.com>
    Change-Id: Icbe1d80f22ff08aebe4e7b299eaaa54a67bf068c

---
+++ update_symbol_list_cloudripper-staging.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-2.0
+BUILD_AOSP_KERNEL=0 \
+BUILD_STAGING_KERNEL=1 \
+GKI_KERNEL_DIR=aosp-staging \
+GKI_KERNEL_REMOTE=partner-common \
+GKI_KERNEL_BRANCH=android13-5.10-pixel-staging \
+BUILD_SCRIPT="./build_cloudripper.sh" \
+DEVICE_KERNEL_BUILD_CONFIG=private/gs-google/build.config.cloudripper \
+private/gs-google/update_symbol_list.sh "$@"
