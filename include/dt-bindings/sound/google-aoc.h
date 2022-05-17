9e9dcf4f25c953840723654abc40853f2bddead3
Robert Lee <lerobert@google.com>
2022-05-17 09:19:34 +0000

    aoc/alsa: dt-bindings: audio capture injection
    Bug: 230110146
    Test: Builds
    Change-Id: I1bd07173bdb54b5eb74edcc858d7852d37bec86a
    Signed-off-by: Robert Lee <lerobert@google.com>

--- include/dt-bindings/sound/google-aoc.h
+++ include/dt-bindings/sound/google-aoc.h
@@ -40,7 +40,8 @@
 #define IDX_HAPTIC_NoHOST_RX	0x20000011
 #define IDX_US_RX		0x13
 #define IDX_INCALL_PB2_RX       0x14
-#define IDX_IMSV_RX              0x15
+#define IDX_IMSV_RX             0x15
+#define IDX_CAP_INJ_RX          0x16
 
 #define IDX_EP1_TX		0x40000000
 #define IDX_EP2_TX		0x40000001
