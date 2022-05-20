6439e8649b3fc4e1a2df341c316d3b3fd70b59f3
SalmaxChang <salmaxchang@google.com>
2022-05-20 21:44:14 +0800

    soc/google/cpif: refine CP reset sequence
    - Pull and keep PMIC WRSTBI low for warm reset until CP full reset.
    
    Bug: 228822582
    Test: Trigger CP crash and reset
    
    Signed-off-by: SalmaxChang <salmaxchang@google.com>
    Change-Id: I95d373d91fa25f284694ae83c1d16f572acd3754

--- drivers/soc/google/cpif/modem_ctrl_s5100.c
+++ drivers/soc/google/cpif/modem_ctrl_s5100.c
@@ -772,7 +772,6 @@ static void gpio_power_wreset_cp(struct modem_ctl *mc)
 #if !IS_ENABLED(CONFIG_CP_WRESET_WA)
 	mif_gpio_set_value(&mc->cp_gpio[CP_GPIO_AP2CP_CP_WRST_N], 0, 50);
 	mif_gpio_set_value(&mc->cp_gpio[CP_GPIO_AP2CP_PM_WRST_N], 0, 50);
-	mif_gpio_set_value(&mc->cp_gpio[CP_GPIO_AP2CP_PM_WRST_N], 1, 50);
 	mif_gpio_set_value(&mc->cp_gpio[CP_GPIO_AP2CP_CP_WRST_N], 1, 50);
 #endif
 }
