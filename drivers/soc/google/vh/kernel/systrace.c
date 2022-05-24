96e3d2911f0548e8af13d6f874dcb992a16d4af2
Luke Chang <lukechang@google.com>
2022-05-24 02:01:40 +0000

    Revert "vh: systrace: add suspend/resume trace as clk trace for visualization"
    This reverts commit 8ea093b027f1aca2be294317a4ff16e497a68bc5.
    
    Revert this patch since suspend_resume event parsing is ready on
    perfetto UI prod line.
    
    Test: take a perfetto trace
    Bug: 229179330
    
    Change-Id: Ie8678651f743b7509aca86709836f16032f8c585
    Signed-off-by: Luke Chang <lukechang@google.com>

--- drivers/soc/google/vh/kernel/systrace.c
+++ drivers/soc/google/vh/kernel/systrace.c
@@ -7,35 +7,18 @@
 
 #include <linux/module.h>
 
-#include <trace/events/power.h>
 #define CREATE_TRACE_POINTS
 #include "systrace.h"
 
 EXPORT_TRACEPOINT_SYMBOL_GPL(0);
 
-#define SUSPEND_CLOCK_STRING_LEN			(32)
-
-static void note_suspend_resume(void *data, const char *action, int val, bool start)
-{
-	if (unlikely(trace_clock_set_rate_enabled())) {
-		char clock_name[SUSPEND_CLOCK_STRING_LEN] = {0};
-		scnprintf(clock_name, SUSPEND_CLOCK_STRING_LEN, "S-%s-%d", action, val);
-		trace_clock_set_rate(clock_name, start, raw_smp_processor_id());
-	}
-}
-
 static int systrace_init(void)
 {
-	int ret = register_trace_suspend_resume(note_suspend_resume, NULL);
-	if (ret)
-		return ret;
-
 	return 0;
 }
 
 static void systrace_exit(void)
 {
-	unregister_trace_suspend_resume(note_suspend_resume, NULL);
 }
 
 module_init(systrace_init);
