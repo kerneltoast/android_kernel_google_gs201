c1605bbba6f0ac795ebddbf1cb7a2065def41809
Kisang Lee <kisang80.lee@samsung.com>
2022-06-08 14:04:15 +0900

    pcie: s51xx: add resume_complete
    Add resume_complete for bme setting.
    when AP resume, set bme in resume_complete.
    
    Bug: 234186881
    
    Signed-off-by: Kisang Lee <kisang80.lee@samsung.com>
    Change-Id: I70f6feee6507437e1bcd090df86a0d32f12060a7

--- drivers/soc/google/cpif/s51xx_pcie.h
+++ drivers/soc/google/cpif/s51xx_pcie.h
@@ -20,6 +20,8 @@ extern void exynos_pcie_rc_register_dump(int ch_num);
 extern void exynos_pcie_rc_print_msi_register(int ch_num);
 extern int exynos_pcie_rc_set_outbound_atu(int ch_num, u32 target_addr, u32 offset, u32 size);
 
+extern u32 pcie_linkup_stat(void);
+
 struct s51xx_pcie {
 	unsigned int busdev_num;
 	int pcie_channel_num;
