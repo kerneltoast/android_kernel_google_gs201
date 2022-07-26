b3b81ae2b06363b9f1c8934e7732ae588e0cb51c
Ray Chi <raychi@google.com>
2022-07-26 14:11:58 +0800

    usb: dwc3: modify disconnect flow
    When kernel is deactivating USB gadget, userspace may start USB
    gadget in the same time. Therefore, it may cause a race condition
    for USB gadget.
    
    This patch will deactivate USB gadget first before sending disconnected
    event to userspace so that userspace starts USB gadget will be late. In
    addition, gadget_driver->disconnect() will be executed during USB gadget
    deactivated, so remove it in dwc3_exynos_gadget_disconnect_proc().
    
    Bug: 238852028
    Signed-off-by: Ray Chi <raychi@google.com>
    Change-Id: I2284f8d0c47de21d1a0a78accab75b5b58373a3e
    (cherry picked from commit a4c99535bb00ef33f062b227c172df4b42cfd813)

--- drivers/usb/dwc3/dwc3-exynos-otg.c
+++ drivers/usb/dwc3/dwc3-exynos-otg.c
@@ -558,13 +558,13 @@ static int dwc3_otg_start_gadget(struct otg_fsm *fsm, int on)
 		exynos->vbus_state = false;
 		del_timer_sync(&exynos->usb_connect_timer);
 
+		ret = usb_gadget_deactivate(dwc->gadget);
+		if (ret < 0)
+			dev_err(dev, "USB gadget deactivate failed with %d\n", ret);
+
 		if (exynos->config.is_not_vbus_pad && exynos_usbdrd_get_ldo_status() &&
-				!dotg->in_shutdown) {
+				!dotg->in_shutdown)
 			dwc3_exynos_gadget_disconnect_proc(dwc);
-			ret = usb_gadget_deactivate(dwc->gadget);
-			if (ret < 0)
-				dev_err(dev, "USB gadget deactivate failed with %d\n", ret);
-		}
 
 		if (exynos->extra_delay)
 			msleep(100);
--- drivers/usb/dwc3/dwc3-exynos.c
+++ drivers/usb/dwc3/dwc3-exynos.c
@@ -443,9 +443,6 @@ void dwc3_exynos_gadget_disconnect_proc(struct dwc3 *dwc)
 	reg &= ~DWC3_DCTL_INITU2ENA;
 	dwc3_writel(dwc->regs, DWC3_DCTL, reg);
 
-	if (dwc->gadget_driver && dwc->gadget_driver->disconnect)
-		dwc->gadget_driver->disconnect(dwc->gadget);
-
 	dwc->gadget->speed = USB_SPEED_UNKNOWN;
 	dwc->setup_packet_pending = false;
 	usb_gadget_set_state(dwc->gadget, USB_STATE_NOTATTACHED);
