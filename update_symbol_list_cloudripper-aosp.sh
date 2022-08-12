084a60c9901137b2a5c477967af80e43dc10cff4
Robin Peng <robinpeng@google.com>
2022-08-12 11:58:37 +0800

    Update build parameter for update_symbol_list_<DEVICE>-aosp.sh
    1) BUILD_AOSP_KERNEL=1
    2) BUILD_STAGING_KERNEL=0
    
    Bug: 239510636
    Signed-off-by: Robin Peng <robinpeng@google.com>
    Change-Id: Ie1391af92b5084c32de0a7854323354c7914e6a7

--- update_symbol_list_cloudripper-aosp.sh
+++ update_symbol_list_cloudripper-aosp.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # SPDX-License-Identifier: GPL-2.0
+BUILD_AOSP_KERNEL=1 \
+BUILD_STAGING_KERNEL=0 \
 BUILD_SCRIPT="./build_cloudripper.sh" \
 DEVICE_KERNEL_BUILD_CONFIG=private/gs-google/build.config.cloudripper \
 private/gs-google/update_symbol_list.sh "$@"
