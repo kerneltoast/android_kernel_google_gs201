fe304f8e1e71212579b1da8678b28ed2fc70a5ab
Kisang Lee <kisang80.lee@samsung.com>
2022-08-26 20:53:11 +0900

    pcie: driver: move state in dislink
    Refine the pcie state update timing to avoid concurrency issue.
    
    Bug: 241954441
    
    Signed-off-by: Kisang Lee <kisang80.lee@samsung.com>
    Change-Id: Ibc305dc72b4dc5483e2dd05b4555528e29acc0aa

--- drivers/pci/controller/dwc/pcie-exynos-rc.c
+++ drivers/pci/controller/dwc/pcie-exynos-rc.c
@@ -3028,8 +3028,8 @@ void exynos_pcie_rc_poweroff(int ch_num)
 		}
 
 		spin_lock_irqsave(&exynos_pcie->conf_lock, flags);
-		exynos_pcie_rc_send_pme_turn_off(exynos_pcie);
 		exynos_pcie->state = STATE_LINK_DOWN;
+		exynos_pcie_rc_send_pme_turn_off(exynos_pcie);
 		power_stats_update_down(exynos_pcie);
 
 		/* Disable SysMMU */
