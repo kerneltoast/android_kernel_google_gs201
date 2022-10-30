079e6076474564b4cf032b935ea1f8d8830f14c4
Martin Liu <liumartin@google.com>
2022-10-30 14:38:03 +0800

    mm: disable drain TLB batching against CMA pages
    It seems we have a chance to run zap_pte_range_tlb for a long time
    while preempt_disable. Disable this first.
    
    Bug: 256275234
    Test: boot
    Change-Id: I9ee4f8c491db2621548d8e7325c919c2fade6aa1
    Signed-off-by: Martin Liu <liumartin@google.com>

--- drivers/soc/google/vh/kernel/mm/vh_mm_init.c
+++ drivers/soc/google/vh/kernel/mm/vh_mm_init.c
@@ -63,18 +63,6 @@ static int vh_mm_init(void)
 			vh_pagevec_drain, NULL);
 	if (ret)
 		return ret;
-	ret = register_trace_android_vh_zap_pte_range_tlb_start(
-			vh_zap_pte_range_tlb_start, NULL);
-	if (ret)
-		return ret;
-	ret = register_trace_android_vh_zap_pte_range_tlb_force_flush(
-			vh_zap_pte_range_tlb_force_flush, NULL);
-	if (ret)
-		return ret;
-	ret = register_trace_android_vh_zap_pte_range_tlb_end(
-			vh_zap_pte_range_tlb_end, NULL);
-	if (ret)
-		return ret;
 	ret = register_trace_android_vh_skip_lru_disable(
 			vh_skip_lru_disable, NULL);
 	if (ret)
