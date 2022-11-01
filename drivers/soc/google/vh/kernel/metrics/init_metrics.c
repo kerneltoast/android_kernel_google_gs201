18285ae4979b59d5de827857cdf8c9f9df00dca5
Ziyi Cui <ziyic@google.com>
2022-11-01 10:35:53 -0700

    drivers: thermal: thermal_metrics: add temperature residency stats logic
    Implement register_temp_residency_stats, temp_residency_stats_update API for temperature residency.
    Add sysfs nodes for the metrics.
    
    Bug: 246799997
    Signed-off-by: Ziyi Cui <ziyic@google.com>
    
    Change-Id: I912757df86f0b008b5fd228e084f695b0eba1634

--- drivers/soc/google/vh/kernel/metrics/init_metrics.c
+++ drivers/soc/google/vh/kernel/metrics/init_metrics.c
@@ -8,15 +8,17 @@
 #include <linux/module.h>
 #include <linux/kobject.h>
 
+extern thermal_metrics_init(struct kobject *metrics_kobj);
 
 struct kobject *metrics_kobj;
 
 static int __init metrics_init(void)
 {
 	metrics_kobj = kobject_create_and_add("metrics", kernel_kobj);
+	thermal_metrics_init(metrics_kobj);
 	return 0;
 }
-EXPORT_SYMBOL_GPL(metrics_kobj);
+
 module_init(metrics_init);
 MODULE_LICENSE("GPL v2");
 MODULE_AUTHOR("Ziyi Cui <ziyic@google.com>");
