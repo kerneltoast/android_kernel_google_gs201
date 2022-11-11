6d3a808eb28c8e59387be8b6b03f7747eb1aea88
Ziyi Cui <ziyic@google.com>
2022-11-10 20:24:50 -0800

    drivers: soc: Add init_metrics module
    Bug: 246799997
    Signed-off-by: Ziyi Cui <ziyic@google.com>
    Change-Id: I429482caf7d2b498f5f1a87c2aeca984d2f0e18b

---
+++ drivers/soc/google/vh/kernel/metrics/init_metrics.c
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/* init_metrics.c
+ *
+ * Support for init metrics
+ * Copyright 2022 Google LLC
+ */
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kobject.h>
+
+
+struct kobject *metrics_kobj;
+
+static int __init metrics_init(void)
+{
+	metrics_kobj = kobject_create_and_add("metrics", kernel_kobj);
+	return 0;
+}
+EXPORT_SYMBOL_GPL(metrics_kobj);
+module_init(metrics_init);
+MODULE_LICENSE("GPL v2");
+MODULE_AUTHOR("Ziyi Cui <ziyic@google.com>");
