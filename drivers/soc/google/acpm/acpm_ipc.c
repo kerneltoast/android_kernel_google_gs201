bc12999c8ec79518e5bf3bcc3004e83c94605726
Redick_Lin <redicklin@google.com>
2022-12-08 11:17:06 +0800

    acpm: add spinlock to protect acpm_debug->ktop_cxt
    Bug: 249004528
    Change-Id: Ic962789288dda03f3f57f3e7be4f0616792916ee
    Signed-off-by: Redick_Lin <redicklin@google.com>

--- drivers/soc/google/acpm/acpm_ipc.c
+++ drivers/soc/google/acpm/acpm_ipc.c
@@ -647,10 +647,34 @@ static void cpu_irq_info_dump(u32 retry)
 
 static void acpm_ktop_release(void)
 {
+	unsigned long flags;
+
+	spin_lock_irqsave(&acpm_debug->lock, flags);
 	if (acpm_debug->ktop_cxt) {
 		kernel_top_destroy(acpm_debug->ktop_cxt);
 		acpm_debug->ktop_cxt = NULL;
 	}
+	spin_unlock_irqrestore(&acpm_debug->lock, flags);
+}
+
+static void acpm_ktop_init(void)
+{
+	unsigned long flags;
+
+	spin_lock_irqsave(&acpm_debug->lock, flags);
+	acpm_debug->ktop_cxt = NULL;
+	kernel_top_init(acpm_ipc->dev, &acpm_debug->ktop_cxt);
+	spin_unlock_irqrestore(&acpm_debug->lock, flags);
+}
+
+static void acpm_ktop_print(void)
+{
+	unsigned long flags;
+
+	spin_lock_irqsave(&acpm_debug->lock, flags);
+	if (acpm_debug->ktop_cxt)
+		kernel_top_print(acpm_debug->ktop_cxt);
+	spin_unlock_irqrestore(&acpm_debug->lock, flags);
 }
 
 int __acpm_ipc_send_data(unsigned int channel_id, struct ipc_config *cfg, bool w_mode)
@@ -784,15 +808,13 @@ retry:
 
 					cpu_irq_info_dump(retry_cnt);
 					if (retry_cnt == 1) {
-						acpm_debug->ktop_cxt = NULL;
-						kernel_top_init(acpm_ipc->dev,
-								&acpm_debug->ktop_cxt);
-
 						acpm_debug->debug_log_level =
 							acpm_debug->retry_log ?
 								2 : saved_debug_log_level;
 						acpm_log_print();
 						acpm_debug->debug_log_level = saved_debug_log_level;
+					} else if (retry_cnt == 2) {
+						acpm_ktop_init();
 					}
 					++retry_cnt;
 
@@ -818,8 +840,8 @@ retry:
 			pr_err("%s Timeout error! now = %llu timeout = %llu ch:%u s:%u bitmap:%lx\n",
 			       __func__, now, timeout, channel->id, seq_num,
 			       channel->bitmap_seqnum[0]);
-			if (acpm_debug->ktop_cxt)
-				kernel_top_print(acpm_debug->ktop_cxt);
+
+			acpm_ktop_print();
 			acpm_ramdump();
 			dump_stack();
 			dbg_snapshot_do_dpm_policy(acpm_ipc->panic_action, "acpm_ipc timeout");
