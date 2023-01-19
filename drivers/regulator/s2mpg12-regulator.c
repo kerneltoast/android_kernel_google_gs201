83236d0031094ff6161fa9a6d1202151cff58969
André Draszik <draszik@google.com>
2023-01-19 10:36:05 +0000

    s2mpg12 s2mpg13: switch to device-managed devm_regulator_register()
    Upstream kernel commit [1] has changed the regulator_register() API.
    
    Update the regulator drivers here to use the device-managed
    devm_regulator_register() instead (which hasn't changed), ensuring
    compatibility with any kernels and simplifying future cherry-picks
    of this driver between different branches.
    
    This change:
    * allows us to compile this driver against the 6.1 kernel
    * fixes a memleak in the probe function's error path as a side-effect
    * simplifies driver removal / cleanup
    
    [1] https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=8f3cbcd6b440032ebc7f7d48a1689dcc70a4eb98
    
    Bug: 266049982
    Change-Id: I17183d315178bf2797b585a428e687ef7a21f114
    Signed-off-by: André Draszik <draszik@google.com>

--- drivers/regulator/s2mpg12-regulator.c
+++ drivers/regulator/s2mpg12-regulator.c
@@ -964,12 +964,12 @@ static int s2mpg12_pmic_probe(struct platform_device *pdev)
 		config.of_node = pdata->regulators[i].reg_node;
 		s2mpg12->opmode[id] = regulators[id].enable_mask;
 
-		s2mpg12->rdev[i] = regulator_register(&regulators[id], &config);
+		s2mpg12->rdev[i] = devm_regulator_register(&pdev->dev,
+							   &regulators[id], &config);
 		if (IS_ERR(s2mpg12->rdev[i])) {
 			ret = PTR_ERR(s2mpg12->rdev[i]);
 			dev_err(&pdev->dev, "regulator init failed for %d\n", i);
-			s2mpg12->rdev[i] = NULL;
-			goto err;
+			return ret;
 		}
 	}
 
@@ -995,25 +995,25 @@ static int s2mpg12_pmic_probe(struct platform_device *pdev)
 	ret = s2mpg12_smpl_warn(s2mpg12, pdata);
 	if (ret < 0) {
 		dev_err(&pdev->dev, "s2mpg12_smpl_warn fail\n");
-		goto err;
+		return ret;
 	}
 
 	ret = s2mpg12_ocp_warn(s2mpg12, pdata);
 	if (ret < 0) {
 		dev_err(&pdev->dev, "s2mpg12_ocp_warn fail\n");
-		goto err;
+		return ret;
 	}
 
 	ret = s2mpg12_set_sel_vgpio(s2mpg12, pdata);
 	if (ret < 0) {
 		dev_err(&pdev->dev, "s2mpg12_set_sel_vgpio fail\n");
-		goto err;
+		return ret;
 	}
 #if IS_ENABLED(TEST_DBG)
 	ret = s2mpg12_oi_function(s2mpg12);
 	if (ret < 0) {
 		dev_err(&pdev->dev, "s2mpg12_oi_function fail\n");
-		goto err;
+		return ret;
 	}
 #endif
 
@@ -1025,22 +1025,13 @@ static int s2mpg12_pmic_probe(struct platform_device *pdev)
 #endif
 
 	return 0;
-err:
-	for (i = 0; i < S2MPG12_REGULATOR_MAX; i++)
-		regulator_unregister(s2mpg12->rdev[i]);
-
-	return ret;
 }
 
 static int s2mpg12_pmic_remove(struct platform_device *pdev)
 {
-	struct s2mpg12_pmic *s2mpg12 = platform_get_drvdata(pdev);
-	int i;
-
-	for (i = 0; i < S2MPG12_REGULATOR_MAX; i++)
-		regulator_unregister(s2mpg12->rdev[i]);
-
 #if IS_ENABLED(CONFIG_DRV_SAMSUNG_PMIC)
+	struct s2mpg12_pmic *s2mpg12 = platform_get_drvdata(pdev);
+
 	pmic_device_destroy(s2mpg12->dev->devt);
 #endif
 	return 0;
