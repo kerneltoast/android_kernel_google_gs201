67ce652415e2f0cd69a93051fe6c8403c1453176
Ting-Hsin Chen <tinghsin@google.com>
2023-03-09 03:13:36 +0000

    Revert "pcie: drivers: add exynos_pcie_msi_set_affinity"
    This reverts commit 57c2e67382433c169ef81eedc79ab4d9252e46a1.
    
    Reason for revert: fail after aosp merge android13-5.10.157
    Bug: 270280369
    
    Change-Id: Ie9457cbf691469019b3d9b694c028779d21f9c35
    Signed-off-by: Ting-Hsin Chen <tinghsin@google.com>

--- drivers/pci/controller/dwc/pcie-exynos-rc.c
+++ drivers/pci/controller/dwc/pcie-exynos-rc.c
@@ -4428,15 +4428,6 @@ static void __maybe_unused exynos_pcie_rc_set_tpoweron(struct pcie_port *pp, int
 static int exynos_pcie_msi_set_affinity(struct irq_data *irq_data, const struct cpumask *mask,
 					bool force)
 {
-	struct pcie_port *pp = irq_data->parent_data->domain->host_data;
-
-	if (pp == NULL) {
-		pr_err("Warning: exynos_pcie_msi_set_affinity: not exist pp\n");
-		return -EINVAL;
-	}
-
-	irq_set_affinity_hint(pp->irq, mask);
-
 	return 0;
 }
 
