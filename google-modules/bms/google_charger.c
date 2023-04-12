dd27c310cb2bd62843b179ed162b578d8873e176
Sultan Alsawaf <sultan@kerneltoast.com>
2023-04-11 23:09:53 -0700

    bms: Fix compilation when debugfs is disabled
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- google-modules/bms/google_charger.c
+++ google-modules/bms/google_charger.c
@@ -35,10 +35,8 @@
 #include "google_dc_pps.h"
 #include "google_psy.h"
 
-#ifdef CONFIG_DEBUG_FS
 #include <linux/debugfs.h>
 #include <linux/seq_file.h>
-#endif
 
 #define CHG_DELAY_INIT_MS		250
 #define CHG_DELAY_INIT_DETECT_MS	1000
@@ -3599,8 +3597,6 @@ static int chg_vote_input_suspend(struct chg_drv *chg_drv,
 	return 0;
 }
 
-#ifdef CONFIG_DEBUG_FS
-
 static int chg_get_input_suspend(void *data, u64 *val)
 {
 	struct chg_drv *chg_drv = (struct chg_drv *)data;
@@ -3869,9 +3865,6 @@ static int bd_enabled_set(void *data, u64 val)
 
 DEFINE_SIMPLE_ATTRIBUTE(bd_enabled_fops, bd_enabled_get,
 			bd_enabled_set, "%lld\n");
-
-
-#endif
 
 static int debug_get_pps_cc_tolerance(void *data, u64 *val)
 {
@@ -5208,8 +5201,6 @@ state2power_table_show(struct device *dev, struct device_attribute *attr, char *
 
 static DEVICE_ATTR_RO(state2power_table);
 
-#ifdef CONFIG_DEBUG_FS
-
 static ssize_t tm_store(struct chg_thermal_device *tdev,
 			const char __user *user_buf,
 			size_t count, loff_t *ppos)
@@ -5278,8 +5269,6 @@ static ssize_t dc_tm_store(struct file *filp, const char __user *user_buf,
 }
 
 DEBUG_ATTRIBUTE_WO(dc_tm);
-
-#endif // CONFIG_DEBUG_FS
 
 static int chg_init_mdis_stats_map(struct chg_drv *chg_drv, const char *name) {
 	int rc, byte_len;
--- google-modules/bms/google_cpm.c
+++ google-modules/bms/google_cpm.c
@@ -3111,8 +3111,6 @@ static const struct thermal_cooling_device_ops chg_mdis_tcd_ops = {
 	.set_cur_state = gcpm_set_mdis_charge_cntl_limit,
 };
 
-#ifdef CONFIG_DEBUG_FS
-
 static ssize_t mdis_tm_store(struct file *filp, const char __user *user_buf,
 			     size_t count, loff_t *ppos)
 {
@@ -3353,8 +3351,6 @@ static int dc_cc_lim_store(void *data, u64 val)
 DEFINE_SIMPLE_ATTRIBUTE(dc_cc_lim_fops, dc_cc_lim_show,
 			dc_cc_lim_store, "%lld\n");
 
-
-#endif // CONFIG_DEBUG_FS
 
 /* ------------------------------------------------------------------------- */
 
