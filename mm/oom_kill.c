389269f685ef2ef6c383df899f56ac872742db70
Sultan Alsawaf <sultan@kerneltoast.com>
2023-05-05 13:20:47 -0700

    mm: Disable process_mrelease syscall when Simple LMK is enabled
    The process_mrelease syscall is fundamentally at odds with Simple LMK
    because Simple LMK is a kernel-based memory pressure solution that performs
    all policy decisions in the kernel, rather than relying on a userspace
    daemon to select victim processes to kill. It can lead to a priority
    inversion where a lower-priority process performing process_mrelease for a
    different process can slow the target process' death by holding the target
    mm's mmap lock while the target process needs it in exit_mmap().
    
    Since process_mrelease can impede Simple LMK's ability to zap processes
    quickly, disable it when Simple LMK is configured.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- mm/oom_kill.c
+++ mm/oom_kill.c
@@ -1225,6 +1225,7 @@ void pagefault_out_of_memory(void)
 		pr_warn("Huh VM_FAULT_OOM leaked out to the #PF handler. Retrying PF\n");
 }
 
+#ifndef CONFIG_ANDROID_SIMPLE_LMK
 SYSCALL_DEFINE2(process_mrelease, int, pidfd, unsigned int, flags)
 {
 #ifdef CONFIG_MMU
@@ -1288,6 +1289,7 @@ put_task:
 	return -ENOSYS;
 #endif /* CONFIG_MMU */
 }
+#endif
 
 void add_to_oom_reaper(struct task_struct *p)
 {
