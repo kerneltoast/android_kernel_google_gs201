80a5dc81d35638861dc43db0fa57ceefb6368b19
Sultan Alsawaf <sultan@kerneltoast.com>
2023-05-08 18:56:19 -0700

    focaltech: Optimize interrupt pm_qos usage
    Instead of waking and preventing all CPUs from entering deep idle states
    every time there's an interrupt, only apply this to the lone CPU servicing
    the touch IRQ. This produces significant power savings without changing the
    performance of the touch IRQ.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- google-modules/touch/focaltech/ft3658/focaltech_core.c
+++ google-modules/touch/focaltech/ft3658/focaltech_core.c
@@ -1954,6 +1954,12 @@ static int fts_irq_registration(struct fts_ts_data *ts_data)
     ts_data->irq = gpio_to_irq(pdata->irq_gpio);
     pdata->irq_gpio_flags = IRQF_TRIGGER_FALLING | IRQF_ONESHOT;
     FTS_INFO("irq:%d, flag:%x", ts_data->irq, pdata->irq_gpio_flags);
+
+    /* init pm_qos before interrupt registered. */
+    ts_data->pm_qos_req.type = PM_QOS_REQ_AFFINE_IRQ;
+    ts_data->pm_qos_req.irq = ts_data->irq;
+    cpu_latency_qos_add_request(&ts_data->pm_qos_req, PM_QOS_DEFAULT_VALUE);
+
     ret = request_threaded_irq(ts_data->irq, fts_irq_ts, fts_irq_handler,
                                pdata->irq_gpio_flags,
                                FTS_DRIVER_NAME, ts_data);
@@ -3102,8 +3108,6 @@ static int fts_ts_probe_entry(struct fts_ts_data *ts_data)
         FTS_ERROR("init esd check fail");
     }
 #endif
-    /* init pm_qos before interrupt registered. */
-    cpu_latency_qos_add_request(&ts_data->pm_qos_req, PM_QOS_DEFAULT_VALUE);
 
     ret = fts_irq_registration(ts_data);
     if (ret) {
