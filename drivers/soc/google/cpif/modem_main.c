07d0c75e763752b020cecad0772be2e1169a71ee
Mahesh Kallelil <kallelil@google.com>
2023-05-10 18:58:10 -0700

    soc/google/cpif: Fix invalid logbuffer pointer use
    If logbuffer_register fails with ERR_PTR, then a call to logbuffer_log
    will cause a kernel panic unless we set the return log struct to NULL.
    
    Bug: 281687359
    Change-Id: I9e0c40f420cfef308d9a39c9e788bbcb3c2b58cc
    Signed-off-by: Mahesh Kallelil <kallelil@google.com>

--- drivers/soc/google/cpif/modem_main.c
+++ drivers/soc/google/cpif/modem_main.c
@@ -778,8 +778,10 @@ static int cpif_probe(struct platform_device *pdev)
 	}
 
 	modemctl->log = logbuffer_register("cpif");
-	if (IS_ERR_OR_NULL(modemctl->log))
+	if (IS_ERR_OR_NULL(modemctl->log)) {
 		mif_err("Failed to register logbuffer!\n");
+		modemctl->log = NULL;
+	}
 
 	if (toe_dev_create(pdev)) {
 		mif_err("%s: toe dev not created\n", pdata->name);
