429b4c832c37b4ea5fdac1fabdd7c664e157792a
Sultan Alsawaf <sultan@kerneltoast.com>
2023-05-10 22:28:00 -0700

    pinctrl: samsung: Implement IRQ affinity control
    Note that for external interrupts, the affinity cannot be configured per
    pin, so interrupts for all pins in a block will have their affinity set.
    This isn't ideal but it's better than nothing.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- google-modules/soc/gs/drivers/pinctrl/gs/pinctrl-exynos.c
+++ google-modules/soc/gs/drivers/pinctrl/gs/pinctrl-exynos.c
@@ -224,6 +224,18 @@ static void exynos_irq_release_resources(struct irq_data *irqd)
 	gpiochip_unlock_as_irq(&bank->gpio_chip, irqd->hwirq);
 }
 
+static int exynos_eint_irq_set_affinity(struct irq_data *irqd,
+					const struct cpumask *dest, bool force)
+{
+	struct samsung_pin_bank *bank = irq_data_get_irq_chip_data(irqd);
+	struct irq_data *parent = irq_get_irq_data(bank->drvdata->irq);
+
+	if (parent && parent->chip->irq_set_affinity)
+		return parent->chip->irq_set_affinity(parent, dest, force);
+
+	return -EINVAL;
+}
+
 /*
  * irq_chip for gpio interrupts.
  * For normal interrupt action, interrupt mask / unmask must be paired.
@@ -242,6 +254,7 @@ static const struct exynos_irq_chip exynos_gpio_irq_chip __initconst = {
 		.irq_set_type = exynos_irq_set_type,
 		.irq_request_resources = exynos_irq_request_resources,
 		.irq_release_resources = exynos_irq_release_resources,
+		.irq_set_affinity = exynos_eint_irq_set_affinity,
 	},
 	.eint_con = EXYNOS_GPIO_ECON_OFFSET,
 	.eint_mask = EXYNOS_GPIO_EMASK_OFFSET,
@@ -490,6 +503,23 @@ s5pv210_pinctrl_set_eint_wakeup_mask(struct samsung_pinctrl_drv_data *drvdata,
 		     clk_base + irq_chip->eint_wake_mask_reg);
 }
 
+static int exynos_wkup_irq_set_affinity(struct irq_data *irqd,
+					const struct cpumask *dest, bool force)
+{
+	struct samsung_pin_bank *bank = irq_data_get_irq_chip_data(irqd);
+	struct exynos_weint_data *eintd = bank->weint_data;
+
+	if (eintd) {
+		struct irq_data *parent = irq_get_irq_data(eintd->irq);
+
+		if (parent && parent->chip->irq_set_affinity)
+			return parent->chip->irq_set_affinity(parent, dest,
+							      force);
+	}
+
+	return -EINVAL;
+}
+
 static u32 eint_wake_mask_value = EXYNOS_EINT_WAKEUP_MASK_DISABLED;
 /*
  * irq_chip for wakeup interrupts
@@ -504,6 +534,7 @@ static const struct exynos_irq_chip s5pv210_wkup_irq_chip __initconst = {
 		.irq_set_wake = exynos_wkup_irq_set_wake,
 		.irq_request_resources = exynos_irq_request_resources,
 		.irq_release_resources = exynos_irq_release_resources,
+		.irq_set_affinity = exynos_wkup_irq_set_affinity,
 	},
 	.eint_con = EXYNOS_WKUP_ECON_OFFSET,
 	.eint_mask = EXYNOS_WKUP_EMASK_OFFSET,
@@ -524,6 +555,7 @@ static const struct exynos_irq_chip exynos4210_wkup_irq_chip __initconst = {
 		.irq_set_wake = exynos_wkup_irq_set_wake,
 		.irq_request_resources = exynos_irq_request_resources,
 		.irq_release_resources = exynos_irq_release_resources,
+		.irq_set_affinity = exynos_wkup_irq_set_affinity,
 	},
 	.eint_con = EXYNOS_WKUP_ECON_OFFSET,
 	.eint_mask = EXYNOS_WKUP_EMASK_OFFSET,
@@ -543,6 +575,7 @@ static const struct exynos_irq_chip exynos7_wkup_irq_chip __initconst = {
 		.irq_set_wake = exynos_wkup_irq_set_wake,
 		.irq_request_resources = exynos_irq_request_resources,
 		.irq_release_resources = exynos_irq_release_resources,
+		.irq_set_affinity = exynos_wkup_irq_set_affinity,
 	},
 	.eint_con = EXYNOS7_WKUP_ECON_OFFSET,
 	.eint_mask = EXYNOS7_WKUP_EMASK_OFFSET,
@@ -572,7 +605,7 @@ static void exynos_irq_eint0_15(struct irq_desc *desc)
 
 	chained_irq_enter(chip, desc);
 
-	generic_handle_domain_irq(bank->irq_domain, eintd->irq);
+	generic_handle_domain_irq(bank->irq_domain, eintd->idx);
 
 	chained_irq_exit(chip, desc);
 }
@@ -683,6 +716,7 @@ __init int exynos_eint_wkup_init(struct samsung_pinctrl_drv_data *d)
 			return -ENOMEM;
 		}
 
+		bank->weint_data = weint_data;
 		for (idx = 0; idx < bank->nr_pins; ++idx) {
 			irq = irq_of_parse_and_map(bank->of_node, idx);
 			if (!irq) {
@@ -690,7 +724,8 @@ __init int exynos_eint_wkup_init(struct samsung_pinctrl_drv_data *d)
 							bank->name, idx);
 				continue;
 			}
-			weint_data[idx].irq = idx;
+			weint_data[idx].idx = idx;
+			weint_data[idx].irq = irq;
 			weint_data[idx].bank = bank;
 			irq_set_chained_handler_and_data(irq,
 							 exynos_irq_eint0_15,
--- google-modules/soc/gs/drivers/pinctrl/gs/pinctrl-samsung.h
+++ google-modules/soc/gs/drivers/pinctrl/gs/pinctrl-samsung.h
@@ -162,6 +162,7 @@ struct samsung_pin_bank_data {
  * @irq_chip: link to irq chip for external gpio and wakeup interrupts.
  * @slock: spinlock protecting bank registers
  * @pm_save: saved register values during suspend
+ * @weint_data: wakeup interrupt data
  */
 struct samsung_pin_bank {
 	const struct samsung_pin_bank_type *type;
@@ -189,6 +190,8 @@ struct samsung_pin_bank {
 	raw_spinlock_t slock;
 
 	u32 pm_save[PINCFG_TYPE_NUM + 1]; /* +1 to handle double CON registers*/
+
+	struct exynos_weint_data *weint_data;
 };
 
 /**
