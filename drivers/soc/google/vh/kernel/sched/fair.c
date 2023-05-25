b27ec6fd59fb9f68bac2a55be77f813dfac5ad9d
Rick Yiu <rickyiu@google.com>
2023-05-25 02:19:43 +0000

    vh: sched: Refine vendor uclamp_eff_get
    To save some cpu cycles.
    
    Bug: 281598730
    Change-Id: Ia624ae096fec92211fe0d2f4b047f3b3636cfcdc
    Signed-off-by: Rick Yiu <rickyiu@google.com>

--- drivers/soc/google/vh/kernel/sched/fair.c
+++ drivers/soc/google/vh/kernel/sched/fair.c
@@ -1371,30 +1371,6 @@ static inline void uclamp_idle_reset(struct rq *rq, enum uclamp_id clamp_id,
 	WRITE_ONCE(rq->uclamp[clamp_id].value, clamp_value);
 }
 
-static inline struct uclamp_se
-uclamp_tg_restrict(struct task_struct *p, enum uclamp_id clamp_id)
-{
-	struct uclamp_se uc_req = p->uclamp_req[clamp_id];
-#ifdef CONFIG_UCLAMP_TASK_GROUP
-	struct uclamp_se uc_max;
-
-	/*
-	 * Tasks in autogroups or root task group will be
-	 * restricted by system defaults.
-	 */
-	if (task_group_is_autogroup(task_group(p)))
-		return uc_req;
-	if (task_group(p) == &root_task_group)
-		return uc_req;
-
-	uc_max = task_group(p)->uclamp[clamp_id];
-	if (uc_req.value > uc_max.value || !uc_req.user_defined)
-		return uc_max;
-#endif
-
-	return uc_req;
-}
-
 /*
  * The effective clamp bucket index of a task depends on, by increasing
  * priority:
@@ -1406,21 +1382,14 @@ uclamp_tg_restrict(struct task_struct *p, enum uclamp_id clamp_id)
 static inline struct uclamp_se
 uclamp_eff_get(struct task_struct *p, enum uclamp_id clamp_id)
 {
-	struct uclamp_se uc_req = uclamp_tg_restrict(p, clamp_id);
 	struct uclamp_se uc_max = uclamp_default[clamp_id];
 	struct uclamp_se uc_eff;
-	int ret = 0;
+	int ret;
 
-	// Instead of calling trace_android_*, call vendor func directly
+	// This function will always return uc_eff
 	rvh_uclamp_eff_get_pixel_mod(NULL, p, clamp_id, &uc_max, &uc_eff, &ret);
-	if (ret)
-		return uc_eff;
 
-	/* System default restrictions always apply */
-	if (unlikely(uc_req.value > uc_max.value))
-		return uc_max;
-
-	return uc_req;
+	return uc_eff;
 }
 
 static inline
