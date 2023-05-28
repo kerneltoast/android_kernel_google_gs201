f396e7bb6366cd54c6fd181379b4f5a03847ba53
Sultan Alsawaf <sultan@kerneltoast.com>
2023-05-28 15:19:29 -0700

    aoc: Block "logging" service since it keeps triggering wakeups
    The "logging" AoC (Always-on Coprocessor) service is wakeup-capable, and it
    triggers wakeups very frequently, thereby disrupting deep sleep on the AP:
    [10839.619254] PM: pm_system_irq_wakeup: 175 triggered 18300000.mbox
    ...
    [10839.666239] aoc 19000000.aoc: Service "logging" has 26 messages to read on wake
    
    This hurts screen-off battery life significantly, which is unacceptable for
    a logging mechanism.
    
    Therefore, block the "logging" service to stop it from triggering frequent
    wakeups. This improves screen-off battery life quite a bit.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- google-modules/aoc/aoc.c
+++ google-modules/aoc/aoc.c
@@ -1210,16 +1210,18 @@ static struct aoc_service_dev *create_service_device(struct aoc_prvdata *prvdata
 	if (!s)
 		return NULL;
 
-	dev = kzalloc(sizeof(struct aoc_service_dev), GFP_KERNEL);
-	if (!dev)
-		return NULL;
-	prvdata->services[index] = dev;
-
 	name = aoc_service_name(s);
 	if (!name)
 		return NULL;
 
 	memcpy_fromio(service_name, name, sizeof(service_name));
+	if (!strcmp(service_name, "logging"))
+		return NULL;
+
+	dev = kzalloc(sizeof(struct aoc_service_dev), GFP_KERNEL);
+	if (!dev)
+		return NULL;
+	prvdata->services[index] = dev;
 
 	dev_set_name(&dev->dev, "%s", service_name);
 	dev->dev.parent = parent;
@@ -1414,15 +1416,15 @@ static void aoc_did_become_online(struct work_struct *work)
 	}
 
 	for (i = 0; i < s; i++) {
-		if (!create_service_device(prvdata, i)) {
-			dev_err(prvdata->dev, "failed to create service device at index %d\n", i);
-			goto err;
-		}
+		create_service_device(prvdata, i);
 	}
 
 	aoc_state = AOC_STATE_ONLINE;
 
 	for (i = 0; i < s; i++) {
+		if (!prvdata->services[i])
+			continue;
+
 		ret = device_register(&prvdata->services[i]->dev);
 		if (ret)
 			dev_err(dev, "failed to register service device %s err=%d\n",
@@ -1656,7 +1658,7 @@ static void aoc_process_services(struct aoc_prvdata *prvdata, int offset)
 	for (i = 0; i < services; i++) {
 		service_dev = service_dev_at_index(prvdata, i);
 		if (!service_dev)
-			goto exit;
+			continue;
 
 		service = service_dev->service;
 		if (service_dev->mbox_index != offset)
