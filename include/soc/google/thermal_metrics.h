f307ec7d35ef00cb4a22bcb5a7d3acf5cfcab6b2
Sultan Alsawaf <sultan@kerneltoast.com>
2023-05-29 19:35:00 -0700

    thermal_metrics: Fix compilation when CONFIG_PIXEL_METRICS=n
    Add static inline definitions for the thermal metrics interfaces to fix
    compilation when CONFIG_PIXEL_METRICS=n.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- include/soc/google/thermal_metrics.h
+++ include/soc/google/thermal_metrics.h
@@ -9,8 +9,20 @@
 #define MAX_SUPPORTED_THRESHOLDS             8
 typedef int tr_handle;
 
+#ifdef CONFIG_PIXEL_METRICS
 int temp_residency_stats_update(tr_handle instance, int temp);
 tr_handle register_temp_residency_stats(const char *name, char *group_name);
 int unregister_temp_residency_stats(tr_handle instance);
 int temp_residency_stats_set_thresholds(tr_handle instance,
                 const int *thresholds, int num_thresholds);
+#else
+static inline
+int temp_residency_stats_update(tr_handle instance, int temp) { return 0; }
+static inline
+tr_handle register_temp_residency_stats(const char *name, char *group_name) { return 0; }
+static inline
+int unregister_temp_residency_stats(tr_handle instance) { return 0; }
+static inline
+int temp_residency_stats_set_thresholds(tr_handle instance,
+                const int *thresholds, int num_thresholds) { return 0; }
+#endif
