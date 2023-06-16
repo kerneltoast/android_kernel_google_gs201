8f9e552ad772080b849aba8b5f20b317257d912e
Sultan Alsawaf <sultan@kerneltoast.com>
2023-06-16 11:03:47 -0700

    media: mfc: Move static arrays out of mfc_core_enc_param.h
    mfc_core_enc_param.h is included by multiple files, while only one file
    actually needs these arrays. Move the arrays to mfc_core_enc_param.c in
    order to fix compile warnings about the arrays being defined but unused.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- google-modules/soc/gs/drivers/media/platform/exynos/mfc/mfc_core_enc_param.h
+++ google-modules/soc/gs/drivers/media/platform/exynos/mfc/mfc_core_enc_param.h
@@ -15,42 +15,6 @@
 
 #include "mfc_common.h"
 
-static int mfc_colorspace_to_rgb_format_ctrl[][2] = {
-	{ MFC_COLORSPACE_UNSPECIFICED,	1}, /* Unknown */
-	{ MFC_COLORSPACE_BT601,		0}, /* Rec. ITU-R BT.601-7 */
-	{ MFC_COLORSPACE_BT709,		1}, /* Rec. ITU-R BT.709-6 */
-	{ MFC_COLORSPACE_SMPTE_170,	0}, /* SMPTE-170 */
-	{ MFC_COLORSPACE_SMPTE_240,	0}, /* SMPTE-240 */
-	{ MFC_COLORSPACE_BT2020,	1}, /* Rec. ITU-R BT.2020-2 */
-	{ MFC_COLORSPACE_RESERVED,	1}, /* Reserved */
-	{ MFC_COLORSPACE_SRGB,		1}, /* sRGB (IEC 61966-2-1) */
-	{ MFC_COLORSPACE_UNSPECIFICED,	1}, /* Unknown */
-	{ MFC_COLORSPACE_UNSPECIFICED,	1}, /* Unknown */
-	{ MFC_COLORSPACE_UNSPECIFICED,	1}, /* Unknown */
-};
-
-static int mfc_transfer_to_rgb_format_ctrl[][2] = {
-	{ MFC_TRANSFER_RESERVED,	1},
-	{ MFC_TRANSFER_BT709,		1},
-	{ MFC_TRANSFER_UNSPECIFIED,	1},
-	{ MFC_TRANSFER_RESERVED,	1},
-	{ MFC_TRANSFER_GAMMA_22,	1},
-	{ MFC_TRANSFER_GAMMA_28,	1},
-	{ MFC_TRANSFER_SMPTE_170M,	0},
-	{ MFC_TRANSFER_SMPTE_240M,	1},
-	{ MFC_TRANSFER_LINEAR,		1},
-	{ MFC_TRANSFER_LOGARITHMIC,	1},
-	{ MFC_TRANSFER_LOGARITHMIC_S,	1},
-	{ MFC_TRANSFER_XvYCC,		1},
-	{ MFC_TRANSFER_BT1361,		1},
-	{ MFC_TRANSFER_SRGB,		1},
-	{ MFC_TRANSFER_BT2020_1,	1},
-	{ MFC_TRANSFER_BT2020_2,	1},
-	{ MFC_TRANSFER_ST2084,		1},
-	{ MFC_TRANSFER_ST428,		1},
-	{ MFC_TRANSFER_HLG,		1},
-};
-
 void mfc_core_set_min_bit_count(struct mfc_core *core, struct mfc_ctx *ctx);
 void mfc_core_set_slice_mode(struct mfc_core *core, struct mfc_ctx *ctx);
 void mfc_core_set_aso_slice_order_h264(struct mfc_core *core,
