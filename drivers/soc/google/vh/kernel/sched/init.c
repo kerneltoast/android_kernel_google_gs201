ef6bae29ea1f373289c03ec1ed2512aac5507d39
Sultan Alsawaf <sultan@kerneltoast.com>
2023-06-18 16:38:34 -0700

    Revert "vh: sched: add sched_newidle_balance"
    This reverts commit 89606f27c4c5e475c2ba0fb8986d18d68b49bb5f.
    
    Causes ~15% regression in single-core performance.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/soc/google/vh/kernel/sched/init.c
+++ drivers/soc/google/vh/kernel/sched/init.c
@@ -76,9 +76,6 @@ extern void vh_try_to_freeze_todo_logging_pixel_mod(void *data, bool *logging_on
 extern void rvh_cpumask_any_and_distribute(void *data, struct task_struct *p,
 	const struct cpumask *cpu_valid_mask, const struct cpumask *new_mask, int *dest_cpu);
 
-void sched_newidle_balance_pixel_mod(void *data, struct rq *this_rq, struct rq_flags *rf,
-		int *pulled_task, int *done);
-
 extern struct cpufreq_governor sched_pixel_gov;
 
 static int vh_sched_init(void)
@@ -146,11 +143,6 @@ static int vh_sched_init(void)
 
 	ret = register_trace_android_rvh_cpu_cgroup_online(
 		rvh_cpu_cgroup_online_pixel_mod, NULL);
-	if (ret)
-		return ret;
-
-	ret = register_trace_android_rvh_sched_newidle_balance(
-		sched_newidle_balance_pixel_mod, NULL);
 	if (ret)
 		return ret;
 
