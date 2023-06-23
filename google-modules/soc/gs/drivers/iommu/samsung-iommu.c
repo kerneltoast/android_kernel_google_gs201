b24eb13c9efe2a2aa2cba44576e8cc66899eb758
Sultan Alsawaf <sultan@kerneltoast.com>
2023-06-22 17:23:56 -0700

    iommu/samsung: Disable fault reporting by default
    Lots of subsystems, such as the TPU, occasionally spam hundreds of
    thousands of IOMMU faults which are not only resource heavy due to the IRQ
    overhead, but also destroy dmesg/ramoops with tons of spam. These errors
    appear to be nonfatal and don't seem actionable for anyone outside of
    Samsung or Google, so turn them off by default.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- google-modules/soc/gs/drivers/iommu/samsung-iommu.c
+++ google-modules/soc/gs/drivers/iommu/samsung-iommu.c
@@ -35,6 +35,8 @@
 #define REG_MMU_S2PF_ENABLE	0x7000
 #define MMU_S2PF_ENABLE		BIT(0)
 
+#define ENABLE_FAULT_REPORTING 0
+
 static const unsigned int sysmmu_reg_set[MAX_SET_IDX][MAX_REG_IDX] = {
 	/* Default without VM */
 	{
@@ -1442,6 +1444,7 @@ static int __sysmmu_secure_irq_init(struct device *sysmmu,
 	}
 	data->secure_irq = ret;
 
+#if ENABLE_FAULT_REPORTING
 	ret = devm_request_threaded_irq(sysmmu, (unsigned int)data->secure_irq,
 					samsung_sysmmu_irq,
 					samsung_sysmmu_irq_thread,
@@ -1451,6 +1454,7 @@ static int __sysmmu_secure_irq_init(struct device *sysmmu,
 			data->secure_irq, ret);
 		return ret;
 	}
+#endif
 
 	ret = of_property_read_u32(sysmmu->of_node, "sysmmu,secure_base",
 				   &data->secure_base);
@@ -1565,6 +1569,7 @@ static int samsung_sysmmu_device_probe(struct platform_device *pdev)
 	if (irq < 0)
 		return irq;
 
+#if ENABLE_FAULT_REPORTING
 	ret = devm_request_threaded_irq(dev, (unsigned int)irq, samsung_sysmmu_irq,
 					samsung_sysmmu_irq_thread,
 					IRQF_ONESHOT, dev_name(dev), data);
@@ -1572,6 +1577,7 @@ static int samsung_sysmmu_device_probe(struct platform_device *pdev)
 		dev_err(dev, "unabled to register handler of irq %d\n", irq);
 		return ret;
 	}
+#endif
 
 	data->clk = devm_clk_get(dev, "gate");
 	if (PTR_ERR(data->clk) == -ENOENT) {
