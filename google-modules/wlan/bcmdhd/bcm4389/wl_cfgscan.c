d4502ad5b1d5dd62cc9b473173be188952ee08e5
Sultan Alsawaf <sultan@kerneltoast.com>
2023-06-30 22:08:25 -0700

    bcmdhd: Disable as much debug bloat as possible
    The overhead from all the debugging in this monstrosity of a driver is
    measurably significant. Chop it all out.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- google-modules/wlan/bcmdhd/bcm4389/wl_cfgscan.c
+++ google-modules/wlan/bcmdhd/bcm4389/wl_cfgscan.c
@@ -4179,9 +4179,11 @@ static void wl_scan_timeout(unsigned long data)
 		return;
 	}
 
+#ifdef DHD_FW_COREDUMP
 	if (dhdp->memdump_enabled) {
 		dhdp->hang_reason = HANG_REASON_SCAN_TIMEOUT;
 	}
+#endif /* DHD_FW_COREDUMP */
 #if defined(DHD_KERNEL_SCHED_DEBUG) && defined(DHD_FW_COREDUMP)
 	/* DHD triggers Kernel panic if the SCAN timeout occurrs
 	 * due to tasklet or workqueue scheduling problems in the Linux Kernel.
