3f9c5ab8c154a90092258686e0fd9e464050222a
Sultan Alsawaf <sultan@kerneltoast.com>
2023-07-30 13:36:41 -0700

    watchdog: s3c2410_wdt: Remove vendor hook unregister
    Now that all vendor hooks are restricted, it's not possible to unregister
    any vendor hook. Remove the unregister from the module unload path.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- google-modules/soc/gs/drivers/watchdog/s3c2410_wdt.c
+++ google-modules/soc/gs/drivers/watchdog/s3c2410_wdt.c
@@ -1904,10 +1904,8 @@ static int s3c2410wdt_remove(struct platform_device *dev)
 
 	unregister_pm_notifier(&s3c2410wdt_pm_nb);
 
-	if (wdt->schedstat) {
-		WARN_ON(unregister_trace_android_vh_scheduler_tick(vh_scheduler_tick, wdt));
+	if (wdt->schedstat)
 		free_percpu(wdt->schedstat);
-	}
 
 	return ret;
 }
