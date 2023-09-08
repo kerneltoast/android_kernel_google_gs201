2af4d6d052743160967028ee17d99f1b1a4038c4
Sultan Alsawaf <sultan@kerneltoast.com>
2023-09-07 23:40:41 -0700

    eh: Use real-time priority for the compression thread
    EH compression is offloaded and the compression thread just does a sleeping
    poll to wait for compression to finish. It's not CPU intensive and many
    higher priority tasks can depend on compression finishing, which results in
    a priority inversion.
    
    Additionally, the ability for zRAM to effectively reduce memory pressure is
    dependent upon how quickly pages are compressed. By limiting the thread to
    SCHED_NORMAL, compressed pages can take arbitrarily long to be released
    back to zRAM.
    
    Furthermore, starving the compression thread results in compressed pages
    lingering in the hardware fifo longer than necessary, quickly filling it up
    and causing EH to be underutilized.
    
    Improve overall EH performance by giving the compression thread real-time
    scheduling priority.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- google-modules/soc/gs/drivers/soc/google/eh/eh_main.c
+++ google-modules/soc/gs/drivers/soc/google/eh/eh_main.c
@@ -709,12 +709,8 @@ static bool ready_to_run(struct eh_device *eh_dev, bool *slept)
 static int eh_comp_thread(void *data)
 {
 	struct eh_device *eh_dev = data;
-	struct sched_attr attr = {
-		.sched_policy = SCHED_NORMAL,
-		.sched_nice = -10,
-	};
 
-	WARN_ON_ONCE(sched_setattr_nocheck(current, &attr) != 0);
+	sched_set_fifo_low(current);
 	current->flags |= PF_MEMALLOC;
 	set_freezable();
 
