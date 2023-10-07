423a518b8af7413c8a275292b2822c77521cfed1
Sultan Alsawaf <sultan@kerneltoast.com>
2023-10-07 15:24:19 -0700

    Squashed 'google-devices/lynx/' content from commit 55e663e722de
    git-subtree-dir: google-devices/lynx
    git-subtree-split: 55e663e722de2b4fad25a05b7cfff9b8dfd14e3a

---
+++ build_lynx.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# SPDX-License-Identifier: GPL-2.0
+DEVICE_KERNEL_BUILD_CONFIG=private/devices/google/lynx/build.config.lynx \
+private/gs-google/build_slider.sh "$@"
---
+++ update_symbol_list_lynx-staging.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-2.0
+BUILD_AOSP_KERNEL=0 \
+BUILD_STAGING_KERNEL=1 \
+GKI_KERNEL_DIR=aosp-staging \
+GKI_KERNEL_REMOTE=partner-common \
+GKI_KERNEL_BRANCH=android13-5.10-pixel-staging \
+BUILD_SCRIPT="./build_lynx.sh" \
+DEVICE_KERNEL_BUILD_CONFIG=private/devices/google/lynx/build.config.lynx \
+private/gs-google/update_symbol_list.sh "$@"
