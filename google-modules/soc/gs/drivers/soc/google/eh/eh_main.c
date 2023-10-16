cf0be2d731a5a7335f7197a5774e17301e2cef93
Sultan Alsawaf <sultan@kerneltoast.com>
2023-10-15 21:35:31 -0700

    eh: Simplify compression thread by removing statistics tracking
    The statistics are unused by userspace and make the compression thread more
    complex. Remove them to simplify the compression thread, making the
    compression thread more robust in the process.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- google-modules/soc/gs/drivers/soc/google/eh/eh_main.c
+++ google-modules/soc/gs/drivers/soc/google/eh/eh_main.c
@@ -646,7 +646,7 @@ static unsigned int eh_wait_next_index(struct eh_device *eh_dev, unsigned int i)
 static int eh_process_compress(struct eh_device *eh_dev)
 {
 	unsigned int i = eh_dev->complete_index, end, index;
-	int nr_handled = 0, ret;
+	int ret;
 
 	/* Flush sw_fifo in case hw_fifo is empty */
 	if (!atomic_read(&eh_dev->nr_request))
@@ -662,7 +662,6 @@ static int eh_process_compress(struct eh_device *eh_dev)
 			ret = eh_process_completed_descriptor(eh_dev, index);
 			if (ret)
 				return ret;
-			nr_handled++;
 			/*
 			 * Since we have available space in hw_fifo, put the
 			 * next compression request immediately from sw_fifo to
@@ -672,7 +671,7 @@ static int eh_process_compress(struct eh_device *eh_dev)
 		} while ((i = (i + 1) & eh_dev->fifo_color_mask) != end);
 	} while (atomic_read(&eh_dev->nr_request));
 
-	return nr_handled;
+	return 0;
 }
 
 static void eh_abort_incomplete_descriptors(struct eh_device *eh_dev)
@@ -694,16 +693,7 @@ static void eh_abort_incomplete_descriptors(struct eh_device *eh_dev)
 	}
 }
 
-static bool ready_to_run(struct eh_device *eh_dev, bool *slept)
-{
-	if (atomic_read(&eh_dev->nr_request) || !sw_fifo_empty(&eh_dev->sw_fifo))
-		return true;
-
-	*slept = true;
-	return false;
-}
-
-static int eh_comp_thread(void *data)
+static int __noreturn eh_comp_thread(void *data)
 {
 	struct eh_device *eh_dev = data;
 
@@ -711,9 +701,8 @@ static int eh_comp_thread(void *data)
 	current->flags |= PF_MEMALLOC;
 	set_freezable();
 
-	while (!kthread_should_stop()) {
+	while (1) {
 		int ret;
-		bool slept = false;
 
 #ifdef CONFIG_SOC_ZUMA
 		/*
@@ -727,15 +716,9 @@ static int eh_comp_thread(void *data)
 		 */
 		exynos_update_ip_idle_status(eh_dev->ip_index, 1);
 #endif
-		wait_event_freezable(eh_dev->comp_wq, ready_to_run(eh_dev, &slept));
-
-		/*
-		 * The condition check above is racy so the schedule
-		 * couldn't schedule out the process but it should be
-		 * rare and the stat doesn't need to be precise.
-		 */
-		if (slept)
-			eh_dev->nr_run++;
+		wait_event_freezable(eh_dev->comp_wq,
+			atomic_read(&eh_dev->nr_request) ||
+			!sw_fifo_empty(&eh_dev->sw_fifo));
 #ifdef CONFIG_SOC_ZUMA
 		exynos_update_ip_idle_status(eh_dev->ip_index, 0);
 #endif
@@ -749,7 +732,7 @@ static int eh_comp_thread(void *data)
 				       error);
 				eh_dump_regs(eh_dev);
 				eh_abort_incomplete_descriptors(eh_dev);
-				break;
+				BUG();
 			}
 
 			/*
@@ -758,14 +741,11 @@ static int eh_comp_thread(void *data)
 			 */
 			WARN_ON(1);
 		}
-
-		eh_dev->nr_compressed += ret;
 	}
 
 #ifdef CONFIG_SOC_ZUMA
 	exynos_update_ip_idle_status(eh_dev->ip_index, 1);
 #endif
-	return 0;
 }
 
 /* Initialize SW related stuff */
@@ -945,24 +925,6 @@ static void eh_hw_deinit(struct eh_device *eh_dev)
 	eh_dev->regs = NULL;
 }
 
-static void eh_sw_deinit(struct eh_device *eh_dev)
-{
-	if (eh_dev->error_irq) {
-		free_irq(eh_dev->error_irq, eh_dev);
-		eh_dev->error_irq = 0;
-	}
-
-	if (eh_dev->comp_irq) {
-		free_irq(eh_dev->comp_irq, eh_dev);
-		eh_dev->comp_irq = 0;
-	}
-
-	if (eh_dev->comp_thread) {
-		kthread_stop(eh_dev->comp_thread);
-		eh_dev->comp_thread = NULL;
-	}
-}
-
 /* Initialize HW related stuff */
 static int eh_hw_init(struct eh_device *eh_dev, unsigned short fifo_size,
 		      phys_addr_t regs, unsigned short quirks)
@@ -1027,51 +989,6 @@ iounmap:
 	return ret;
 }
 
-#define EH_ATTR_RO(_name) \
-	static struct kobj_attribute _name##_attr = __ATTR_RO(_name)
-
-static ssize_t nr_run_show(struct kobject *kobj,
-		struct kobj_attribute *attr,
-		char *buf)
-{
-	struct eh_device *eh_dev = container_of(kobj, struct eh_device, kobj);
-
-	return sysfs_emit(buf, "%lu\n", eh_dev->nr_run);
-}
-EH_ATTR_RO(nr_run);
-
-static ssize_t nr_compressed_show(struct kobject *kobj,
-		struct kobj_attribute *attr,
-		char *buf)
-{
-	struct eh_device *eh_dev = container_of(kobj, struct eh_device, kobj);
-
-	return sysfs_emit(buf, "%lu\n", eh_dev->nr_compressed);
-}
-EH_ATTR_RO(nr_compressed);
-
-static struct attribute *eh_attrs[] = {
-	&nr_run_attr.attr,
-	&nr_compressed_attr.attr,
-	NULL,
-};
-ATTRIBUTE_GROUPS(eh);
-
-static void eh_kobj_release(struct kobject *kobj)
-{
-	struct eh_device *eh_dev = container_of(kobj, struct eh_device, kobj);
-
-	eh_sw_deinit(eh_dev);
-	eh_hw_deinit(eh_dev);
-	kfree(eh_dev);
-}
-
-static struct kobj_type eh_ktype = {
-	.release = eh_kobj_release,
-	.sysfs_ops = &kobj_sysfs_ops,
-	.default_groups = eh_groups,
-};
-
 /* EmeraldHill initialization entry */
 static int eh_init(struct device *device, struct eh_device *eh_dev,
 		   unsigned short fifo_size, phys_addr_t regs, int error_irq,
@@ -1096,12 +1013,7 @@ static int eh_init(struct device *device, struct eh_device *eh_dev,
 		return ret;
 	}
 
-	ret = kobject_init_and_add(&eh_dev->kobj, &eh_ktype,
-				   kernel_kobj, "%s", "eh");
-	if (ret)
-		kobject_put(&eh_dev->kobj);
-
-	return ret;
+	return 0;
 }
 
 static void eh_setup_dcmd(struct eh_device *eh_dev, unsigned int index,
@@ -1366,8 +1278,6 @@ static int eh_of_remove(struct platform_device *pdev)
 	clk_put(eh_dev->clk);
 	pm_runtime_put_sync(&pdev->dev);
 	pm_runtime_disable(&pdev->dev);
-
-	kobject_put(&eh_dev->kobj);
 	return 0;
 }
 
