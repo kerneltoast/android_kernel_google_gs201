0dcc4e106ac21518fa84ac521d1dab433a2dc6c5
Sultan Alsawaf <sultan@kerneltoast.com>
2024-01-05 16:49:24 -0800

    bcmdhd: Fix bss memory leak in wl_cfg80211_verify_bss()
    Although wl_cfg80211_verify_bss() claims that the bss reference it takes
    will be put in cfg80211_connect_bss() on >=4.7 kernels, this isn't true and
    instead the bss reference is leaked, leaking the entire bss struct.
    
    This happens because a new reference on the same bss is taken before it is
    passed along to cfg80211_connect_done() in wl_fillup_conn_resp_params().
    Note that cfg80211_connect_bss() is just a cfg80211_connect_done() wrapper.
    
    Since wl_fillup_conn_resp_params() always takes a reference to the bss
    before passing it to cfg80211_connect_done(), this ends up leaving the bss
    with an extra reference from wl_cfg80211_verify_bss() that's never put.
    
    This results in the bss struct getting leaked along with any ies structs
    referenced within the bss:
    unreferenced object 0xffffff88a430fb00 (size 256):
      comm "wpa_supplicant", pid 6572, jiffies 4294905639 (age 15.108s)
      hex dump (first 32 bytes):
        00 fb 30 a4 88 ff ff ff 00 fb 30 a4 88 ff ff ff  ..0.......0.....
        10 fb 30 a4 88 ff ff ff 10 fb 30 a4 88 ff ff ff  ..0.......0.....
      backtrace:
        [<000000002e820356>] __kmalloc+0x138/0x214
        [<000000006f40334a>] cfg80211_bss_update+0x11c/0x564
        [<000000003345db45>] cfg80211_inform_bss_frame_data+0x1c4/0x774
        [<00000000705b6a5d>] wl_inform_single_bss+0x34c/0x47c
        [<00000000fb0eca0c>] wl_notify_escan_complete.isra.0+0xd4/0x360
        [<00000000879d7515>] _wl_cfgscan_cancel_scan+0x154/0x1fc
        [<0000000065f0aa74>] wl_cfg80211_scan_stop+0x34/0x60
        [<000000009389492f>] wl_cfgp2p_stop_p2p_device+0xe4/0x170
        [<000000000937ae88>] cfg80211_stop_p2p_device+0x4c/0xe0
        [<00000000e90319a1>] nl80211_stop_p2p_device+0x30/0x4c
        [<000000000fa521af>] genl_rcv_msg+0x168/0x330
        [<00000000bdb39ce3>] netlink_rcv_skb+0x5c/0x130
        [<000000001ec28a2e>] genl_rcv+0x38/0x50
        [<000000004cb5083d>] netlink_unicast+0x250/0x390
        [<00000000ea821cf4>] netlink_sendmsg+0x1cc/0x40c
        [<0000000057ee7df6>] ____sys_sendmsg+0x258/0x2c4
    unreferenced object 0xffffff890207b800 (size 512):
      comm "kworker/u19:6", pid 1700, jiffies 4294906333 (age 12.332s)
      hex dump (first 32 bytes):
        83 bb 58 03 00 00 00 00 00 00 00 00 00 00 00 00  ..X.............
        00 00 00 00 00 00 00 00 13 01 00 00 00 00 12 47  ...............G
      backtrace:
        [<000000002e820356>] __kmalloc+0x138/0x214
        [<000000009ced4ede>] cfg80211_inform_bss_frame_data+0xe4/0x774
        [<00000000705b6a5d>] wl_inform_single_bss+0x34c/0x47c
        [<00000000fb0eca0c>] wl_notify_escan_complete.isra.0+0xd4/0x360
        [<00000000e1b91eab>] wl_escan_handler+0x900/0xfd0
        [<00000000c73c83e3>] wl_event_handler+0x1fc/0x21c
        [<0000000030bfa43a>] process_one_work+0x1b0/0x324
        [<00000000c2c01f78>] worker_thread+0x70/0x4ac
        [<0000000001163e9e>] kthread+0x16c/0x180
        [<000000000fbd3547>] ret_from_fork+0x10/0x20
    
    Fix the bss leak by unconditionally putting the bss reference acquired in
    wl_cfg80211_verify_bss().
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- google-modules/wlan/bcm4389/wl_cfg80211.c
+++ google-modules/wlan/bcm4389/wl_cfg80211.c
@@ -14958,12 +14958,7 @@ wl_cfg80211_verify_bss(struct bcm_cfg80211 *cfg, struct net_device *ndev,
 	*bss = CFG80211_GET_BSS(wiphy, NULL, curbssid,
 		ssid->SSID, ssid->SSID_len);
 	if (*bss) {
-#if (LINUX_VERSION_CODE < KERNEL_VERSION(4, 7, 0))
-		/* Update the reference count after use. In case of kernel version >= 4.7
-		* the cfg802_put_bss is called in cfg80211_connect_bss context
-		*/
 		CFG80211_PUT_BSS(wiphy, *bss);
-#endif /* LINUX_VERSION_CODE < KERNEL_VERSION(4, 7, 0) */
 		ret = true;
 	} else {
 		WL_ERR(("No bss entry for bssid:"MACDBG" ssid_len:%d\n",
