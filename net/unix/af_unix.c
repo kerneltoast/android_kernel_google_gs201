1a31765c4bfa50edd5744c97a905b06b4ad62a7e
Sultan Alsawaf <sultan@kerneltoast.com>
2024-02-10 22:51:30 -0800

    af_unix: Block libperfmgr from writing to the logd socket
    The logcat spam from libperfmgr when the kernel doesn't have everything it
    wants destroys logcats and creates excessive logd overhead from the sheer
    number of logs it prints.
    
    Block libperfmgr from writing to the logd socket so that it cannot write to
    logcat at all. This way, all libperfmgr attempts to write to logcat are
    killed early on before they can incur overhead, which solves the excessive
    logd overhead problem.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- net/unix/af_unix.c
+++ net/unix/af_unix.c
@@ -1331,6 +1331,7 @@ static void unix_state_double_unlock(struct sock *sk1, struct sock *sk2)
 	unix_state_unlock(sk2);
 }
 
+bool task_is_libperfmgr(struct task_struct *p);
 static int unix_dgram_connect(struct socket *sock, struct sockaddr *addr,
 			      int alen, int flags)
 {
@@ -1347,6 +1348,11 @@ static int unix_dgram_connect(struct socket *sock, struct sockaddr *addr,
 		err = unix_validate_addr(sunaddr, alen);
 		if (err)
 			goto out;
+
+		/* Block libperfmgr from writing to logd (i.e., logcat) */
+		if (task_is_libperfmgr(current) &&
+		    !strncmp(sunaddr->sun_path, "/dev/socket/logdw", alen))
+			return -EINVAL;
 
 		if (test_bit(SOCK_PASSCRED, &sock->flags) &&
 		    !unix_sk(sk)->addr) {
