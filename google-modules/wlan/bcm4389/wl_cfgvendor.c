59a7511aae20dc6899da0bd94b3383a7df8dc6fa
Sultan Alsawaf <sultan@kerneltoast.com>
2024-03-31 20:23:46 -0700

    bcmdhd: Fix missing break in wl_cfgvendor_twt_setup() switch statement
    The trigger type case in wl_cfgvendor_twt_setup() erroneously falls through
    to the wake duration case, setting `val.desc.wake_dur` when it shouldn't.
    
    Fix it by adding the missing break to the ANDR_TWT_ATTR_TRIGGER_TYPE case.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- google-modules/wlan/bcm4389/wl_cfgvendor.c
+++ google-modules/wlan/bcm4389/wl_cfgvendor.c
@@ -10716,7 +10716,7 @@ wl_cfgvendor_twt_setup(struct wiphy *wiphy,
 				if (nla_get_u8(iter) == 1) {
 					val.desc.flow_flags |= WL_TWT_FLOW_FLAG_TRIGGER;
 				}
-				BCM_FALLTHROUGH;
+				break;
 			case ANDR_TWT_ATTR_WAKE_DURATION:
 				/* Wake Duration */
 				val.desc.wake_dur = nla_get_u32(iter);
