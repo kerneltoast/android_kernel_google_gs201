56df1b4503e633deb40c421d62ab7adfed82f3a5
Sultan Alsawaf <sultan@kerneltoast.com>
2024-09-26 18:29:08 -0700

    gs_panel: Silence display brightness log spam
    This goes off every time the display brightness changes. Silence it.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- google-modules/display/common/gs_panel/gs_panel.c
+++ google-modules/display/common/gs_panel/gs_panel.c
@@ -1193,7 +1193,7 @@ static int gs_update_backlight_status(struct backlight_device *bl)
 		dev_dbg(dev, "bl range is changed to %d\n", bl_range);
 	}
 
-	dev_info(dev, "req: %d, br: %d\n", bl->props.brightness, brightness);
+	dev_dbg(dev, "req: %d, br: %d\n", bl->props.brightness, brightness);
 
 	mutex_unlock(&ctx->mode_lock); /*TODO(b/267170999): MODE*/
 	PANEL_ATRACE_END(__func__);
