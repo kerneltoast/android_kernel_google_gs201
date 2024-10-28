e06351fb3176651ea7edb14f31c3cdc7cc63fa9b
Sultan Alsawaf <sultan@kerneltoast.com>
2024-10-27 22:18:16 -0700

    kernel: Support integrated module loading from first stage init
    Android's first stage init emits a different message when it finishes
    loading all modules:
      [   19.069683] init: Loaded 187 kernel modules took 17611 ms
    
    Update check_modules_init_done() to support module loading from first stage
    init. Future messages can be added by expanding `init_done_msgs` now.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- include/linux/module.h
+++ include/linux/module.h
@@ -75,6 +75,7 @@ extern struct module_attribute module_uevent;
 /* These are either module local, or the kernel's dummy ones. */
 extern int init_module(void);
 extern void cleanup_module(void);
+extern bool integrated_module_load_in_progress(void);
 extern void integrated_module_load_end(void);
 
 #if !defined(MODULE) || defined(CONFIG_INTEGRATE_MODULES)
