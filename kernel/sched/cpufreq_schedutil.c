70c0d279676d9594d1ae4721786da6716b69a6f1
Sultan Alsawaf <sultan@kerneltoast.com>
2024-11-29 14:40:52 -0800

    cpufreq: schedutil: Set default rate limit to 2000 us
    This is empirically observed to yield good performance with reduced power
    consumption. With "cpufreq: schedutil: Ignore rate limit when scaling up
    with FIE present", this only affects frequency reductions when FIE is
    present, since there is no rate limit applied when scaling up.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -744,7 +744,7 @@ static int sugov_init(struct cpufreq_policy *policy)
 		goto stop_kthread;
 	}
 
-	tunables->rate_limit_us = cpufreq_policy_transition_delay_us(policy);
+	tunables->rate_limit_us = 2000;
 
 	policy->governor_data = sg_policy;
 	sg_policy->tunables = tunables;
