a9900b42e424490945ca95efeedac96ff71962d3
Sultan Alsawaf <sultan@kerneltoast.com>
2024-12-02 22:38:28 -0800

    soc/google/cpif: Don't pin workers onto CPU2
    There's no reason to pin workers onto CPU2. Instead, allow the workers to
    run where the workqueue sees fit, most likely on the CPU which enqueues the
    worker, which is better than always dumping the workers onto CPU2.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- google-modules/radio/samsung/s5300/modem_ctrl_s5100.c
+++ google-modules/radio/samsung/s5300/modem_ctrl_s5100.c
@@ -54,8 +54,6 @@ static int s5100_lcd_notifier(struct notifier_block *notifier,
 
 #define msecs_to_loops(t) (loops_per_jiffy / 1000 * HZ * t)
 
-#define RUNTIME_PM_AFFINITY_CORE 2
-
 static struct modem_ctl *g_mc;
 
 static int s5100_poweroff_pcie(struct modem_ctl *mc, bool force_off);
@@ -349,8 +347,7 @@ static irqreturn_t ap_wakeup_handler(int irq, void *data)
 		(gpio_val == 1 ? IRQF_TRIGGER_LOW : IRQF_TRIGGER_HIGH));
 	mif_enable_irq(&mc->cp_gpio_irq[CP_GPIO_IRQ_CP2AP_WAKEUP]);
 
-	queue_work_on(RUNTIME_PM_AFFINITY_CORE, mc->wakeup_wq,
-			(gpio_val == 1 ? &mc->wakeup_work : &mc->suspend_work));
+	queue_work(mc->wakeup_wq, gpio_val == 1 ? &mc->wakeup_work : &mc->suspend_work);
 
 	return IRQ_HANDLED;
 }
@@ -2008,8 +2005,7 @@ static int s5100_pm_notifier(struct notifier_block *notifier,
 				(gpio_val == 1 ? IRQF_TRIGGER_LOW : IRQF_TRIGGER_HIGH));
 			mif_enable_irq(&mc->cp_gpio_irq[CP_GPIO_IRQ_CP2AP_WAKEUP]);
 
-			queue_work_on(RUNTIME_PM_AFFINITY_CORE, mc->wakeup_wq,
-				(gpio_val == 1 ? &mc->wakeup_work : &mc->suspend_work));
+			queue_work(mc->wakeup_wq, gpio_val == 1 ? &mc->wakeup_work : &mc->suspend_work);
 		}
 		spin_unlock_irqrestore(&mc->pcie_pm_lock, flags);
 		break;
@@ -2167,15 +2163,13 @@ static int s5100_call_state_notifier(struct notifier_block *nb,
 			cpif_wake_unlock(mc->ws_wrst);
 			logbuffer_log(mc->log, "released wrst wakelock after voice call");
 		}
-		queue_work_on(RUNTIME_PM_AFFINITY_CORE, mc->wakeup_wq,
-			&mc->call_off_work);
+		queue_work(mc->wakeup_wq, &mc->call_off_work);
 		break;
 	case MODEM_VOICE_CALL_ON:
 		mc->pcie_voice_call_on = true;
 		if (mc->mdm_data->mif_off_during_volte)
 			mif_enable_irq(&mc->cp_gpio_irq[CP_GPIO_IRQ_CP2AP_CP_WRST_N]);
-		queue_work_on(RUNTIME_PM_AFFINITY_CORE, mc->wakeup_wq,
-			&mc->call_on_work);
+		queue_work(mc->wakeup_wq, &mc->call_on_work);
 		break;
 	default:
 		mif_err("undefined call event = %lu\n", action);
--- google-modules/radio/samsung/s5300/s51xx_pcie.c
+++ google-modules/radio/samsung/s5300/s51xx_pcie.c
@@ -342,7 +342,7 @@ static void s51xx_pcie_event_cb(struct exynos_pcie_notify *noti)
 
 		if (mc->pcie_linkdown_retry_cnt++ < 10) {
 			mif_err("[%d] retry pcie poweron !!!\n", mc->pcie_linkdown_retry_cnt);
-			queue_work_on(2, mc->wakeup_wq, &mc->wakeup_work);
+			queue_work(mc->wakeup_wq, &mc->wakeup_work);
 		} else {
 			mif_err("[%d] force crash !!!\n", mc->pcie_linkdown_retry_cnt);
 			exynos_pcie_rc_dump_all_status(mc->pcie_ch_num);
@@ -354,7 +354,7 @@ static void s51xx_pcie_event_cb(struct exynos_pcie_notify *noti)
 
 		if (mc->pcie_cto_retry_cnt++ < 10) {
 			mif_err("[%d] retry pcie poweron !!!\n", mc->pcie_cto_retry_cnt);
-			queue_work_on(2, mc->wakeup_wq, &mc->wakeup_work);
+			queue_work(mc->wakeup_wq, &mc->wakeup_work);
 		} else {
 			mif_err("[%d] force crash !!!\n", mc->pcie_cto_retry_cnt);
 			exynos_pcie_rc_dump_all_status(mc->pcie_ch_num);
