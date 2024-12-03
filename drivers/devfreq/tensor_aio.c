52e199f95bc74571e815033e29f842c018972d85
Sultan Alsawaf <sultan@kerneltoast.com>
2024-12-03 15:07:01 -0800

    tensor_aio: Avoid racing with CPU hotplug in memperfd
    When a CPU is hotplugged, cpu_active_mask is modified without any RCU
    synchronization. As a result, the only synchronization for cpu_active_mask
    provided by the hotplug code is the CPU hotplug lock.
    
    Take the CPU hotplug lock in memperfd to ensure a stable view of active
    CPUs in the system during a memperfd run.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/devfreq/tensor_aio.c
+++ drivers/devfreq/tensor_aio.c
@@ -859,6 +859,7 @@ static bool memperf_work(void)
 	int cpu;
 
 	/* Only consider active CPUs */
+	cpus_read_lock();
 	cpumask_copy(&cpus, cpu_active_mask);
 
 	/* Get the current MIF freq, since something else could've raised it */
@@ -928,6 +929,7 @@ static bool memperf_work(void)
 		pmu->prev = pmu->cur;
 		raw_spin_unlock_irq(&pmu->lock);
 	}
+	cpus_read_unlock();
 
 	return ret;
 }
