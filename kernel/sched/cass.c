73865f5ed462623ebcfda99e2ea7e27f82bbff28
Sultan Alsawaf <sultan@kerneltoast.com>
2025-02-05 20:01:57 -0800

    sched/cass: Don't pack tasks with uclamp boosts below minimum CPU capacity
    To save energy, CASS may prefer non-idle CPUs for uclamp-boosted tasks in
    order to pack them onto a single performance domain rather than spreading
    them across multiple performance domains. This way, it is more likely for
    only one performance domain to be boosted a higher P-state when there is
    more than one uclamp-boosted task running.
    
    However, when a task has a uclamp boost value that is below a CPU's minimum
    capacity, it is nearly the same thing as not having a uclamp boost at all.
    
    In spite of that, CASS may still prefer non-idle CPUs for tasks with bogus
    uclamp boost values. This is not only worse for latency, but also energy
    efficiency since the load on the CPU is spread less evenly as a result.
    
    Therefore, don't pack tasks with uclamp boosts below a CPU's minimum
    configured capacity, since such tasks do not force the CPU to run at a
    higher P-state.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- kernel/sched/cass.c
+++ kernel/sched/cass.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
- * Copyright (C) 2023-2024 Sultan Alsawaf <sultan@kerneltoast.com>.
+ * Copyright (C) 2023-2025 Sultan Alsawaf <sultan@kerneltoast.com>.
  */
 
 /**
@@ -209,14 +209,16 @@ static int cass_best_cpu(struct task_struct *p, int prev_cpu, bool sync, bool rt
 		    available_idle_cpu(cpu) || sched_idle_cpu(cpu)) {
 			/*
 			 * A non-idle candidate may be better for energy
-			 * efficiency when @p is uclamp boosted, or when the
-			 * only idle candidate found so far is the prime CPU.
-			 * Otherwise, prefer idle candidates.
+			 * efficiency when @p is uclamp boosted above @curr's
+			 * minimum capacity, or when the only idle candidate
+			 * found so far is the prime CPU. Otherwise, prefer idle
+			 * candidates.
 			 */
-			if (!uc_min && !cass_prime_cpu(curr)) {
+			if (!has_idle &&
+			    uc_min <= arch_scale_min_freq_capacity(cpu) &&
+			    !cass_prime_cpu(curr)) {
 				/* Discard any previous non-idle candidate */
-				if (!has_idle)
-					best = curr;
+				best = curr;
 				has_idle = true;
 			}
 
