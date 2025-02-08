319c217075f8f20454bcaee7a041a99b9f54e60f
Sultan Alsawaf <sultan@kerneltoast.com>
2025-02-07 19:24:18 -0800

    exynos-bts: Convert btsdev->mutex_lock into an rt_mutex lock
    The DPU (display) performs BTS voting from decon0_kthread, which has
    real-time scheduling priority. The ISP (camera) performs BTS voting from
    tasks of varying _normal_ scheduling priority, _not_ real-time scheduling
    priority.
    
    As a result, when decon0_kthread needs to wait for btsdev->mutex_lock to be
    unlocked, there is a priority inversion where the real-time decon0_kthread
    task must wait for the lower-priority mutex owner to finish using the lock.
    
    Fix the priority inversion by converting btsdev->mutex_lock into an
    rt_mutex lock.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- google-modules/soc/gs/drivers/bts/exynos-bts.c
+++ google-modules/soc/gs/drivers/bts/exynos-bts.c
@@ -550,7 +550,7 @@ int bts_update_bw(unsigned int index, struct bts_bw bw)
 		goto err;
 	}
 
-	mutex_lock(&btsdev->mutex_lock);
+	rt_mutex_lock(&btsdev->mutex_lock);
 	bts_bw[index].peak = bw.peak;
 	bts_bw[index].read = bw.read;
 	bts_bw[index].write = bw.write;
@@ -574,7 +574,7 @@ int bts_update_bw(unsigned int index, struct bts_bw bw)
 
 	bts_calc_bw();
 	bts_update_stats(index);
-	mutex_unlock(&btsdev->mutex_lock);
+	rt_mutex_unlock(&btsdev->mutex_lock);
 
 	return 0;
 
@@ -729,7 +729,7 @@ static int exynos_bts_bw_open_show(struct seq_file *buf, void *d)
 {
 	int i;
 
-	mutex_lock(&btsdev->mutex_lock);
+	rt_mutex_lock(&btsdev->mutex_lock);
 	for (i = 0; (btsdev->bts_bw[i].name != NULL) &&
 		(i < btsdev->num_bts); i++) {
 		seq_printf(
@@ -739,7 +739,7 @@ static int exynos_bts_bw_open_show(struct seq_file *buf, void *d)
 			btsdev->bts_bw[i].write, btsdev->bts_bw[i].peak,
 			btsdev->bts_bw[i].rt);
 	}
-	mutex_unlock(&btsdev->mutex_lock);
+	rt_mutex_unlock(&btsdev->mutex_lock);
 	return 0;
 }
 
@@ -753,7 +753,7 @@ static int exynos_bts_bw_hist_open_show(struct seq_file *buf, void *d)
 {
 	int i, j;
 
-	mutex_lock(&btsdev->mutex_lock);
+	rt_mutex_lock(&btsdev->mutex_lock);
 	seq_printf(buf, "Total BW, Count:\nkB/s:\t");
 	for (i = 0; i < BTS_HIST_BIN - 1; i++) {
 		seq_printf(
@@ -846,7 +846,7 @@ static int exynos_bts_bw_hist_open_show(struct seq_file *buf, void *d)
 		}
 		seq_printf(buf, "\n");
 	}
-	mutex_unlock(&btsdev->mutex_lock);
+	rt_mutex_unlock(&btsdev->mutex_lock);
 	return 0;
 }
 
@@ -861,7 +861,7 @@ static ssize_t bts_stats_show(struct device *dev, struct device_attribute *attr,
 	int i, j;
 	ssize_t ret = 0;
 
-	mutex_lock(&btsdev->mutex_lock);
+	rt_mutex_lock(&btsdev->mutex_lock);
 	ret += scnprintf(buf + ret, PAGE_SIZE - ret,
 			"Total BW, Time in ms:\nkB/s:\t");
 	for (i = 0; i < BTS_HIST_BIN - 1; i++) {
@@ -918,7 +918,7 @@ static ssize_t bts_stats_show(struct device *dev, struct device_attribute *attr,
 		}
 		ret += scnprintf(buf + ret, PAGE_SIZE - ret, "\n");
 	}
-	mutex_unlock(&btsdev->mutex_lock);
+	rt_mutex_unlock(&btsdev->mutex_lock);
 	return ret;
 }
 
@@ -2084,7 +2084,7 @@ static int bts_probe(struct platform_device *pdev)
 		return ret;
 	}
 	spin_lock_init(&btsdev->lock);
-	mutex_init(&btsdev->mutex_lock);
+	rt_mutex_init(&btsdev->mutex_lock);
 	INIT_LIST_HEAD(&btsdev->scen_node);
 
 	ret = bts_initialize(btsdev);
